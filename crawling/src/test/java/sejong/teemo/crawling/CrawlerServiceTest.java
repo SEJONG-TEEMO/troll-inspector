dff705e2eda572b40d2b57358d0f3d362905cf73
swager253 <swager253@naver.com>
2024-04-13 14:21:08 +0900

    test: 크롤링 테스트

---
+++ crawling/src/test/java/sejong/teemo/crawling/CrawlerServiceTest.java
@@ -0,0 +1,19 @@
+package sejong.teemo.crawling;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import sejong.teemo.crawling.service.CrawlerService;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class CrawlerServiceTest {
+
+    private static final String url = "https://www.op.gg/leaderboards/tier?page=";
+
+    @Test
+    void 크롤링_테스트() {
+        CrawlerService crawlerService = new CrawlerService(url);
+
+        crawlerService.crawler();
+    }
+}
