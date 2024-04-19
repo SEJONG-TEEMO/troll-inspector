ce97f93a1b24308c70847dac52c8ab96693d0565
swager253 <swager253@naver.com>
2024-04-19 11:06:41 +0900

    feat: 크롤링 비동기 병렬 처리

--- crawling/src/main/java/sejong/teemo/crawling/service/CrawlerService.java
+++ crawling/src/main/java/sejong/teemo/crawling/service/CrawlerService.java
@@ -5,7 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.RemoteWebDriverBuilder;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +18,7 @@ import sejong.teemo.crawling.pool.WebDriverPool;
 import sejong.teemo.crawling.pool.WebDriverPoolingFactory;
 import sejong.teemo.crawling.util.ParserUtil;
 
+import java.net.URL;
 import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.*;
@@ -32,32 +36,30 @@ public class CrawlerService {
         this.url = url;
     }
 
-    public void crawler() {
+    public void crawler(int startPage, int endPage, int maxPoolSize) {
         log.info("url = {}", url);
 
         FirefoxOptions options = new FirefoxOptions();
-        options.addArguments("--disable-popup-blocking");       //팝업안띄움
+        //options.addArguments("--disable-popup-blocking");       //팝업안띄움
         //options.addArguments("--headless");                       //브라우저 안띄움
-        options.addArguments("--disable-gpu");			//gpu 비활성화
-        options.addArguments("--blink-settings=imagesEnabled=false"); //이미지 다운 안받음
+        //options.addArguments("--disable-gpu");			//gpu 비활성화
+        //options.addArguments("--blink-settings=imagesEnabled=false"); //이미지 다운 안받음
+        
+        WebDriverPool webDriverPool = new WebDriverPool(new WebDriverPoolingFactory(options), maxPoolSize);
 
-        WebDriverPool webDriverPool = new WebDriverPool(new WebDriverPoolingFactory(options), 5);
+        webDriverPool.addObjects(maxPoolSize);
 
-        webDriverPool.addObjects(5);
-
-        ExecutorService executorService = Executors.newFixedThreadPool(64);
-        List<CompletableFuture<List<Summoner>>> futures = IntStream.rangeClosed(1, 100)
+        ExecutorService executorService = Executors.newFixedThreadPool(maxPoolSize);
+        List<CompletableFuture<List<Summoner>>> futures = IntStream.rangeClosed(startPage, endPage)
                 .mapToObj(page -> CompletableFuture.supplyAsync(() -> {
-                    log.info("webDriver borrow");
+                    log.info("page = {}", page);
                     WebDriver webDriver = webDriverPool.borrowWebDriver();
-                    log.info("webDriver");
 
                     webDriver.get(url + page);
 
                     // 랭킹 정보가 표시된 테이블 요소 선택 (XPath 또는 CSS Selector 사용)
-                    //WebElement rankingTable = webDriver.findElement(By.cssSelector(".css-1l95r9q > tbody"));
                     WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-                    WebElement rankingTable = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".css-1l95r9q > tbody")));
+                    WebElement rankingTable = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("tbody")));
 
                     // 테이블에서 각 플레이어의 랭킹 정보 추출
                     List<Summoner> summoners = rankingTable.findElements(By.cssSelector("tr"))
@@ -68,15 +70,15 @@ public class CrawlerService {
 
                     webDriverPool.returnObject(webDriver);
                     return summoners;
-                }, executorService)).toList();
+                }, executorService).exceptionally(ex -> {
+                    throw new RuntimeException(ex.getMessage());
+                })).toList();
 
         List<List<Summoner>> result = futures.parallelStream()
                 .map(CompletableFuture::join)
                 .toList();
 
-        result.forEach(summoners -> {
-            log.info("summoners = {}", summoners);
-        });
+        log.info("result = {}", result.size());
 
         webDriverPool.clear();
         webDriverPool.close();
