0ed11341e9aa0f0fcf0543a93b9bf661537d3c79
swager253 <swager253@naver.com>
2024-04-21 17:17:05 +0900

    feat: 환경 변수 프로퍼티 스프링 빈 등록

--- crawling/src/main/java/sejong/teemo/crawling/CrawlingApplication.java
+++ crawling/src/main/java/sejong/teemo/crawling/CrawlingApplication.java
@@ -2,7 +2,10 @@ package sejong.teemo.crawling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class CrawlingApplication {
 
