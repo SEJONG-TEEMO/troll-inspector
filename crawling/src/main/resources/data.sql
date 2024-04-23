66aad4cb074e8e3cd863c8b505829c5397eb837f
swager253 <swager253@naver.com>
2024-04-23 21:40:59 +0900

    feat: schema.sql, data.sql 추가

---
+++ crawling/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+delete from summoner where id > 30000;
--- crawling/src/main/resources/schema.sql
+++ crawling/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE summoner
 (
-    id           BIGINT NOT NULL,
+    id           BIGINT AUTO_INCREMENT NOT NULL,
     name         VARCHAR(255)          NOT NULL,
     tag          VARCHAR(255)          NOT NULL,
     tier         VARCHAR(20)           NOT NULL,
@@ -8,7 +8,9 @@ CREATE TABLE summoner
     level        INT                   NOT NULL,
     wins         INT                   NOT NULL,
     losses       INT                   NOT NULL,
-    create_at    datetime              NULL,
-    update_at    datetime              NULL,
+    create_at    datetime              NOT NULL,
+    update_at    datetime              NOT NULL,
     CONSTRAINT pk_summoner PRIMARY KEY (id)
 );
+
+CREATE INDEX name_tag_idx ON summoner (name, tag);
