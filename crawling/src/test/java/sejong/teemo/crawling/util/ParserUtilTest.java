2e9f54600cf186b728bd775a2a91f021c8aed9a0
swager253 <swager253@naver.com>
2024-04-23 21:44:27 +0900

    test: 파싱 테스트 클래스 추가

---
+++ crawling/src/test/java/sejong/teemo/crawling/util/ParserUtilTest.java
@@ -0,0 +1,11 @@
+package sejong.teemo.crawling.util;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ParserUtilTest {
+}
