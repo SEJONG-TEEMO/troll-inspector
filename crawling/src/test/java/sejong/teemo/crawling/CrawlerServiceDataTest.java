fa956d0ebb1d89f59274eb98f30054e164f0c328
swager253 <swager253@naver.com>
2024-04-29 16:35:50 +0900

    test: 분산 서버 크롤링 테스트

--- crawling/src/test/java/sejong/teemo/crawling/CrawlerServiceDataTest.java
+++ crawling/src/test/java/sejong/teemo/crawling/CrawlerServiceDataTest.java
@@ -3,32 +3,50 @@ package sejong.teemo.crawling;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import sejong.teemo.crawling.facade.CrawlerFacade;
 import sejong.teemo.crawling.property.CrawlingProperties;
+import sejong.teemo.crawling.property.CrawlingPropertiesV1;
+import sejong.teemo.crawling.property.CrawlingPropertiesV2;
 import sejong.teemo.crawling.repository.CrawlerRepository;
 import sejong.teemo.crawling.service.CrawlerService;
+
+import java.util.List;
 
 @SpringBootTest
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class CrawlerServiceDataTest {
 
     @Autowired
-    private FirefoxOptions options;
-
-    @Autowired
     private CrawlerRepository crawlerRepository;
 
     @Autowired
-    private CrawlingProperties crawlingProperties;
+    private CrawlingPropertiesV1 crawlingProperties;
+
+    @Autowired
+    private CrawlingPropertiesV2 crawlingPropertiesV2;
+
+    @Autowired
+    private List<CrawlingProperties> properties;
 
     @Autowired
     private CrawlerService crawlerService;
 
+    @Autowired
+    private CrawlerFacade crawlerFacade;
+
     @Test
     void 크롤링_테스트() {
 
-        crawlerService.crawler(320, 400, 5);
+        crawlerService.crawler(crawlingProperties,2801, 2900, 5);
+    }
+
+    @Test
+    void 분산_크롤링() {
+        int[] startPage = {4601, 4651};
+        int[] endPage = {4650, 4700};
+
+        crawlerFacade.distributeCrawling(startPage, endPage);
     }
 }
--- crawling/src/test/java/sejong/teemo/crawling/CrawlerServiceTest.java
+++ crawling/src/test/java/sejong/teemo/crawling/CrawlerServiceTest.java
@@ -1,36 +1,56 @@
 package sejong.teemo.crawling;
 
+import jakarta.annotation.PostConstruct;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import sejong.teemo.crawling.facade.CrawlerFacade;
 import sejong.teemo.crawling.property.CrawlingProperties;
+import sejong.teemo.crawling.property.CrawlingPropertiesV1;
+import sejong.teemo.crawling.property.CrawlingPropertiesV2;
 import sejong.teemo.crawling.repository.CrawlerRepository;
 import sejong.teemo.crawling.service.CrawlerService;
 
+import java.util.List;
+
 class CrawlerServiceTest extends TestContainer {
-
-    private static final Logger log = LoggerFactory.getLogger(CrawlerServiceTest.class);
-
-    @Autowired
-    private FirefoxOptions options;
 
     @Autowired
     private CrawlerRepository crawlerRepository;
 
     @Autowired
-    private CrawlingProperties crawlingProperties;
+    private CrawlingPropertiesV1 crawlingPropertiesV1;
+
+    @Autowired
+    private CrawlingPropertiesV2 crawlingPropertiesV2;
+
+    @Autowired
+    private List<CrawlingProperties> list;
 
     @Autowired
     private CrawlerService crawlerService;
 
+    @Autowired
+    private CrawlerFacade crawlerFacade;
+
     @Test
     void 크롤링_테스트() {
 
-        log.info("url = {}", crawlingProperties.url());
-        log.info("remoteIp = {}", crawlingProperties.remoteIp());
+        crawlerService.crawler(crawlingPropertiesV1,1, 100, 5);
+    }
 
-        crawlerService.crawler(1, 100, 5);
+    @Test
+    void 분산_크롤링_테스트() {
+        // given
+        int[] startPage = {1, 51};
+        int[] endPage = {50, 100};
+
+        // when
+        crawlerFacade.distributeCrawling(startPage, endPage);
+
+        // then
+
     }
 }
