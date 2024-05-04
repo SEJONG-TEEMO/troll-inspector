d01d550f19189c12ab02c0a56b2b8163dfa8cb46
swager253 <swager253@naver.com>
2024-05-04 16:53:20 +0900

    rename: ExceptionProvider로 클래스 네임 변경

--- crawling/src/main/java/sejong/teemo/crawling/exception/ExceptionProvider.java
+++ crawling/src/main/java/sejong/teemo/crawling/exception/ExceptionProvider.java
@@ -3,14 +3,14 @@ package sejong.teemo.crawling.exception;
 import lombok.Getter;
 
 @Getter
-public enum ExceptionController {
+public enum ExceptionProvider {
     SUMMONER_NOT_FOUND(1000, "SUMMONER_001", "해당 소환사를 찾을 수 없습니다.");
 
     private final int statusCode;
     private final String code;
     private final String message;
 
-    ExceptionController(int statusCode, String code, String message) {
+    ExceptionProvider(int statusCode, String code, String message) {
         this.code = code;
         this.statusCode = statusCode;
         this.message = message;
