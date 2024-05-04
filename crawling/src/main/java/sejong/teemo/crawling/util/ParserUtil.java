25da031fa1054276bc5e290de1dfd6d8182118fc
swager253 <swager253@naver.com>
2024-05-04 16:53:52 +0900

    feat: 매치 데이터 DTO 추가

--- crawling/src/main/java/sejong/teemo/crawling/util/ParserUtil.java
+++ crawling/src/main/java/sejong/teemo/crawling/util/ParserUtil.java
@@ -2,8 +2,10 @@ package sejong.teemo.crawling.util;
 
 import lombok.extern.slf4j.Slf4j;
 import sejong.teemo.crawling.domain.Summoner;
+import sejong.teemo.crawling.dto.MatchDataDto;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @Slf4j
 public class ParserUtil {
@@ -16,6 +18,46 @@ public class ParserUtil {
         //log.info("subLine: {}", (Object) subLine);
 
         return isTierMapping(lines, subLine);
+    }
+
+    public static MatchDataDto parseMatchData(String matchStr) {
+
+        String[] lines = matchStr.split("\n");
+
+        String[] killDeathAssist = lines[5].split("/");
+        String[] ratings = lines[6].split(":");
+        String[] killParticipation = lines[7].split(" ");
+        String[] controlWards = lines[8].split(" ");
+        String[] totalMinionsKilled = lines[9].split(" ");
+
+        log.info("kill death assist: {}", Arrays.toString(killDeathAssist));
+        log.info("rating: {}", Arrays.toString(ratings));
+        log.info("killParticipation: {}", Arrays.toString(killParticipation));
+        log.info("controlWards: {}", Arrays.toString(controlWards));
+        log.info("totalMinionsKilled: {}", Arrays.toString(totalMinionsKilled));
+
+        return isMappingMatchData(killDeathAssist, ratings, killParticipation, controlWards, totalMinionsKilled);
+    }
+
+    private static MatchDataDto isMappingMatchData(String[] killDeathAssist, String[] ratings, String[] killParticipation, String[] controlWards, String[] totalMinionsKilled) {
+
+        String kills = killDeathAssist[0].trim();
+        String deaths = killDeathAssist[1].trim();
+        String assists = killDeathAssist[2].trim();
+        String rating = ratings[0];
+        String userKillParticipation = killParticipation[1].substring(0, killParticipation[1].length() - 1);
+        String controlWard = controlWards[2].trim();
+        String totalMinions = totalMinionsKilled[1];
+
+        return MatchDataDto.builder()
+                .kills(Integer.parseInt(kills))
+                .deaths(Integer.parseInt(deaths))
+                .assists(Integer.parseInt(assists))
+                .rating(Double.parseDouble(rating))
+                .killParticipation(Integer.parseInt(userKillParticipation))
+                .controlWardsPlaced(Integer.parseInt(controlWard))
+                .totalMinionKilled(Integer.parseInt(totalMinions))
+                .build();
     }
 
     private static Summoner isTierMapping(String[] lines, String[] subLine) {
@@ -50,6 +92,10 @@ public class ParserUtil {
         }
 
         return builder.build();
+    }
+
+    public static String skipString(String target, String str) {
+        return str.replaceAll(target, str);
     }
 
     private static int extractTotalGames(String line) {
