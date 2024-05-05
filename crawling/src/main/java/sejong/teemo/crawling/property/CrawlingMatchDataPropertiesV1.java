98b429f8e3d01b5c0772451ff68a62135a37e977
swager253 <swager253@naver.com>
2024-05-05 16:29:51 +0900

    fix: 의존성 주입할 때 프로퍼티 성격이 달라 에러 이슈가 발생 implements 삭제

--- crawling/src/main/java/sejong/teemo/crawling/property/CrawlingMatchDataPropertiesV1.java
+++ crawling/src/main/java/sejong/teemo/crawling/property/CrawlingMatchDataPropertiesV1.java
@@ -3,7 +3,7 @@ package sejong.teemo.crawling.property;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "crawling-match-v1")
-public record CrawlingMatchDataPropertiesV1(String url, String remoteIp) implements CrawlingProperties {
+public record CrawlingMatchDataPropertiesV1(String url, String remoteIp) {
 
     @Override
     public String toString() {
