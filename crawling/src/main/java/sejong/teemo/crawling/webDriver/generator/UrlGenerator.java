03f03fccf9052dd70107cc81d3375c3e2c8f5db8
swager253 <swager253@naver.com>
2024-05-06 15:41:42 +0900

    feat: url 를 생성해주는 enum class 기능 추가

---
+++ crawling/src/main/java/sejong/teemo/crawling/webDriver/generator/UrlGenerator.java
@@ -0,0 +1,31 @@
+package sejong.teemo.crawling.webDriver.generator;
+
+public enum UrlGenerator {
+
+    RIOT_LEADER_BOARD("https://www.op.gg/leaderboards/tier?page="),
+    RIOT_SUMMONERS("https://www.op.gg/summoners/kr/");
+
+    private final String url;
+
+    UrlGenerator(String url) {
+        this.url = url;
+    }
+
+    public String generateUrl(String subUrl) {
+        return this.url + subUrl;
+    }
+
+    public String generateUrl(int subUrl) {
+        return this.url + subUrl;
+    }
+
+    public String generateUrl(String... subUrls) {
+        StringBuilder sb = new StringBuilder();
+
+        for(String subUrl : subUrls) {
+             sb.append(subUrl);
+        }
+
+        return this.url + sb;
+    }
+}
