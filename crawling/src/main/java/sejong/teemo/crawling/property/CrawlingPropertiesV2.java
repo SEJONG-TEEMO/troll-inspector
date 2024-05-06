091933f6c3bba37a2198c57197e91100e1a10731
swager253 <swager253@naver.com>
2024-05-06 15:46:27 +0900

    refactor: 기존 url 파라미터 삭제

--- crawling/src/main/java/sejong/teemo/crawling/property/CrawlingPropertiesV2.java
+++ crawling/src/main/java/sejong/teemo/crawling/property/CrawlingPropertiesV2.java
@@ -3,5 +3,5 @@ package sejong.teemo.crawling.property;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "crawling-v2")
-public record CrawlingPropertiesV2(String url, String remoteIp) implements CrawlingProperties {
+public record CrawlingPropertiesV2(String remoteIp) implements CrawlingProperties {
 }
