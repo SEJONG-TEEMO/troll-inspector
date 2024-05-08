da1c3b179d0ce26b9d66b46ac418e7ae37e71135
swager253 <swager253@naver.com>
2024-05-08 22:31:36 +0900

    feat: 비동기 크롤러 추가

---
+++ crawling/src/main/java/sejong/teemo/crawling/crawler/AsyncCrawler.java
@@ -0,0 +1,53 @@
+package sejong.teemo.crawling.crawler;
+
+import lombok.Builder;
+import org.openqa.selenium.WebDriver;
+import sejong.teemo.crawling.exception.CrawlingException;
+import sejong.teemo.crawling.webDriver.pool.WebDriverPool;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.function.BiFunction;
+import java.util.stream.IntStream;
+
+public class AsyncCrawler<T> {
+
+    private final WebDriverPool pool;
+    private final ExecutorService executor;
+
+    private int startPage = 0, endPage = 0;
+
+    @Builder
+    public AsyncCrawler(WebDriverPool pool, ExecutorService executor) {
+        this.pool = pool;
+        this.executor = executor;
+    }
+
+    public AsyncCrawler<T> setPages(int startPage, int endPage) {
+        this.startPage = startPage;
+        this.endPage = endPage;
+        return this;
+    }
+
+    public AsyncCrawler<T> poolSize(int maxSize) {
+        pool.addObjects(maxSize);
+        return this;
+    }
+
+    public List<List<T>> action(BiFunction<WebDriver, Integer, List<T>> function) {
+        List<CompletableFuture<List<T>>> futures = IntStream.rangeClosed(startPage, endPage)
+                .mapToObj(pageNum -> CompletableFuture.supplyAsync(() -> {
+                    WebDriver webDriver = pool.borrowWebDriver();
+
+                    List<T> list = function.apply(webDriver, pageNum);
+
+                    pool.returnObject(webDriver);
+                    return list;
+                }, executor).exceptionally(ex -> {
+                    throw new CrawlingException(ex.getMessage());
+                })).toList();
+
+        return futures.stream().map(CompletableFuture::join).toList();
+    }
+}
