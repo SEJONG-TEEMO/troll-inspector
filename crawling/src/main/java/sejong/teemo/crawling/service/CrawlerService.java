26247193c36e478fad3efd581aacb888bad725f0
swager253 <swager253@naver.com>
2024-05-08 22:32:48 +0900

    refactor: 비동기 크롤링 서비스 비대한 코드를 모듈화 하여 비대해진 서비스 코드를 개선

--- crawling/src/main/java/sejong/teemo/crawling/service/CrawlerService.java
+++ crawling/src/main/java/sejong/teemo/crawling/service/CrawlerService.java
@@ -2,24 +2,19 @@ package sejong.teemo.crawling.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sejong.teemo.crawling.crawler.Crawler;
+import sejong.teemo.crawling.crawler.AsyncCrawler;
 import sejong.teemo.crawling.crawler.page.LeaderBoardPage;
 import sejong.teemo.crawling.crawler.page.Page;
-import sejong.teemo.crawling.crawler.page.PageImpl;
-import sejong.teemo.crawling.crawler.page.SummonerPage;
+import sejong.teemo.crawling.crawler.page.MatchDataPage;
+import sejong.teemo.crawling.crawler.page.Pages;
 import sejong.teemo.crawling.domain.Summoner;
 import sejong.teemo.crawling.dto.MatchDataDto;
 import sejong.teemo.crawling.exception.CrawlingException;
-import sejong.teemo.crawling.mapper.CrawlerMapperMatchData;
-import sejong.teemo.crawling.mapper.CrawlerMapperSummoner;
 import sejong.teemo.crawling.webDriver.generator.UrlGenerator;
 import sejong.teemo.crawling.webDriver.pool.WebDriverPool;
 import sejong.teemo.crawling.webDriver.pool.WebDriverPoolingFactory;
@@ -27,10 +22,11 @@ import sejong.teemo.crawling.property.CrawlingProperties;
 import sejong.teemo.crawling.repository.CrawlerRepository;
 
 import java.net.URI;
-import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.stream.IntStream;
+
+import static sejong.teemo.crawling.webDriver.generator.UrlGenerator.RIOT_LEADER_BOARD;
 
 @Service
 @Slf4j
@@ -42,36 +38,25 @@ public class CrawlerService {
     @Transactional
     public void crawler(UrlGenerator url, CrawlingProperties properties, int startPage, int endPage, int maxPoolSize) {
         log.info("url = {}", url);
-        
-        WebDriverPool webDriverPool = new WebDriverPool(new WebDriverPoolingFactory(properties), maxPoolSize);
 
-        webDriverPool.addObjects(maxPoolSize);
+        try (WebDriverPool webDriverPool = new WebDriverPool(new WebDriverPoolingFactory(properties), maxPoolSize)) {
+            ExecutorService executorService = Executors.newFixedThreadPool(maxPoolSize);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(maxPoolSize);
-        List<CompletableFuture<List<Summoner>>> futures = IntStream.rangeClosed(startPage, endPage)
-                .mapToObj(pageNum -> CompletableFuture.supplyAsync(() -> {
-                    log.info("page = {}", pageNum);
-                    WebDriver webDriver = webDriverPool.borrowWebDriver();
+            Pages<Summoner> pages = new LeaderBoardPage();
 
-                    Page<Summoner> page = new LeaderBoardPage();
-                    List<Summoner> list = page.crawler(webDriver, url, String.valueOf(pageNum));
+            List<List<Summoner>> lists = pages.asyncCrawler(url, webDriverPool, executorService, startPage, endPage, maxPoolSize);
 
-                    webDriverPool.returnObject(webDriver);
-                    return list;
-                }, executorService).exceptionally(ex -> {
-                    throw new CrawlingException(ex.getMessage());
-                })).toList();
+            executorService.shutdown();
+            webDriverPool.close();
 
-        insertCrawlingData(futures);
-
-        executorService.shutdown();
-        webDriverPool.close();
+            insertCrawlingData(lists);
+        } catch (Exception e) {
+            throw new CrawlingException(e.getMessage());
+        }
     }
 
-    private void insertCrawlingData(List<CompletableFuture<List<Summoner>>> futures) {
-        futures.stream()
-                .map(CompletableFuture::join)
-                .forEach(crawlerRepository::bulkInsert);
+    private void insertCrawlingData(List<List<Summoner>> summonerList) {
+        summonerList.forEach(crawlerRepository::bulkInsert);
     }
 
     public List<MatchDataDto> crawlingMatchData(UrlGenerator url, CrawlingProperties properties, String name, String tag) {
@@ -79,7 +64,7 @@ public class CrawlerService {
         try {
             WebDriver webDriver = new RemoteWebDriver(new URI(properties.remoteIp()).toURL(), new FirefoxOptions());
 
-            Page<MatchDataDto> page = new SummonerPage();
+            Page<MatchDataDto> page = new MatchDataPage();
             List<MatchDataDto> list = page.crawler(webDriver, url, name + "-" + tag);
 
             webDriver.close();
