5b1d02cddab67cf40578540022c537702e27d930
swager253 <swager253@naver.com>
2024-05-08 22:31:57 +0900

    feat: 여러 페이지를 크롤링하는 인터페이스 추가

---
+++ crawling/src/main/java/sejong/teemo/crawling/crawler/page/Pages.java
@@ -0,0 +1,12 @@
+package sejong.teemo.crawling.crawler.page;
+
+import sejong.teemo.crawling.webDriver.generator.UrlGenerator;
+import sejong.teemo.crawling.webDriver.pool.WebDriverPool;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+public interface Pages<T> {
+
+    List<List<T>> asyncCrawler(UrlGenerator url, WebDriverPool pool, ExecutorService executorService, int startPage, int endPage, int maxSize);
+}
