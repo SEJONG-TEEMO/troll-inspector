9328f1efd0a954a1008b6cece97c3374172f3fce
swager253 <swager253@naver.com>
2024-05-08 22:33:10 +0900

    feat: 비동기 크롤러 overriding

--- crawling/src/main/java/sejong/teemo/crawling/crawler/page/LeaderBoardPage.java
+++ crawling/src/main/java/sejong/teemo/crawling/crawler/page/LeaderBoardPage.java
@@ -4,15 +4,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import sejong.teemo.crawling.crawler.AsyncCrawler;
 import sejong.teemo.crawling.crawler.Crawler;
 import sejong.teemo.crawling.domain.Summoner;
 import sejong.teemo.crawling.mapper.CrawlerMapperSummoner;
 import sejong.teemo.crawling.webDriver.generator.UrlGenerator;
+import sejong.teemo.crawling.webDriver.pool.WebDriverPool;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
-public class LeaderBoardPage implements Page<Summoner> {
+import static sejong.teemo.crawling.webDriver.generator.UrlGenerator.RIOT_LEADER_BOARD;
+
+public class LeaderBoardPage implements Page<Summoner>, Pages<Summoner> {
 
     @Override
     public List<Summoner> crawler(WebDriver webDriver, UrlGenerator url, String subUrl) {
@@ -29,5 +34,16 @@ public class LeaderBoardPage implements Page<Summoner> {
                         .map(new CrawlerMapperSummoner()::map)
                         .toList()
                 );
+    }
+
+    @Override
+    public List<List<Summoner>> asyncCrawler(UrlGenerator url, WebDriverPool pool, ExecutorService executorService, int startPage, int endPage, int maxSize) {
+        return AsyncCrawler.<Summoner>builder()
+                .pool(pool)
+                .executor(executorService)
+                .build()
+                .setPages(startPage, endPage)
+                .poolSize(maxSize)
+                .action((webDriver, pageNum) -> new LeaderBoardPage().crawler(webDriver, RIOT_LEADER_BOARD, String.valueOf(pageNum)));
     }
 }
