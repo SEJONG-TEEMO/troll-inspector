69d0718bf01738aa998ebbaf13c9f48e0400a8fb
swager253 <swager253@naver.com>
2024-05-13 15:23:44 +0900

    refactor: footer 간격 설정

--- troll-inspector-view/src/footer/Bottom.jsx
+++ troll-inspector-view/src/footer/Bottom.jsx
@@ -1,19 +1,19 @@
-import {Textarea} from "@nextui-org/react";
-
 export default function bottom() {
     return (
-        <footer className="col-6">
+        <footer id="footer">
             <div>
-                <strong>TEEMO.GG</strong>
-            </div>
-            <div className="mt-5 mb-5 text-center">
-                Copyright(c)2024 by TEEMO.GG
-            </div>
-            <div>
-                <hr className="mb-5"/>
-                <h6>
-                    TEEMO.GG isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
-                </h6>
+                <div className="px-3 py-3 mb-5">
+                    <strong>TEEMO.GG</strong>
+                </div>
+                <hr />
+                <div className="px-5 py-3 p-6 flex items-center justify-center">
+                    <p>
+                        TEEMO.GG isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or
+                        anyone officially involved in producing or managing League of Legends. League of Legends and Riot
+                        Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games,
+                        Inc.
+                    </p>
+                </div>
             </div>
         </footer>
     )
