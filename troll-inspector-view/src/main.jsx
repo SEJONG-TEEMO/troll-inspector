6bcbdd50f8d5dd30c6cb73be8f246bf785ea879d
swager253 <swager253@naver.com>
2024-05-13 15:24:09 +0900

    feat: router 경로 설정

--- troll-inspector-view/src/main.jsx
+++ troll-inspector-view/src/main.jsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {NextUIProvider} from "@nextui-org/react";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <NextUIProvider>
-          <App/>
+          <RouterProvider router={router} />
       </NextUIProvider>
   </React.StrictMode>,
 )
