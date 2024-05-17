54eba49d0e2d363188c48fd3f7acd4fcd8d9abbe
swager253 <swager253@naver.com>
2024-05-17 22:31:16 +0900

    test: 테스트 책임 분리

---
+++ ingame-search/src/test/java/sejong/teemo/ingamesearch/extension/TestExtension.java
@@ -0,0 +1,28 @@
+package sejong.teemo.ingamesearch.extension;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.test.context.ActiveProfiles;
+import sejong.teemo.ingamesearch.dto.Account;
+import sejong.teemo.ingamesearch.dto.Spectator;
+import sejong.teemo.ingamesearch.property.ApikeyProperties;
+
+import java.util.List;
+
+@EnableConfigurationProperties(ApikeyProperties.class)
+@ActiveProfiles("test")
+@RestClientTest
+public class TestExtension {
+
+    protected String getAccount() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(new Account("PUUID", "a1h", "KR1"));
+    }
+
+    protected String getSpectator() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(new Spectator("type", List.of()));
+    }
+}
