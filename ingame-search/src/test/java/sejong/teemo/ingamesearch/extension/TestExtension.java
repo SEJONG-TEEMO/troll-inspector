647093aa3e333494a8a270b4fa5dc7fcd734e866
swager253 <swager253@naver.com>
2024-05-18 22:29:13 +0900

    feat: @DisplayNameGeneration 추가

--- ingame-search/src/test/java/sejong/teemo/ingamesearch/extension/TestExtension.java
+++ ingame-search/src/test/java/sejong/teemo/ingamesearch/extension/TestExtension.java
@@ -2,6 +2,8 @@ package sejong.teemo.ingamesearch.extension;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -14,6 +16,7 @@ import java.util.List;
 @EnableConfigurationProperties(ApikeyProperties.class)
 @ActiveProfiles("test")
 @RestClientTest
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class TestExtension {
 
     protected String getAccount() throws JsonProcessingException {
