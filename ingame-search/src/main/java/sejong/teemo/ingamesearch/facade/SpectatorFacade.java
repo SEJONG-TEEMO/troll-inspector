446eac7eaef421214f9692e29614f946a212411f
swager253 <swager253@naver.com>
2024-05-18 15:35:05 +0900

    feat: spectator 비동기 처리

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/facade/SpectatorFacade.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/facade/SpectatorFacade.java
@@ -2,9 +2,14 @@ package sejong.teemo.ingamesearch.facade;
 
 import lombok.RequiredArgsConstructor;
 import sejong.teemo.ingamesearch.dto.Account;
+import sejong.teemo.ingamesearch.dto.ChampionMastery;
+import sejong.teemo.ingamesearch.dto.CurrentGameParticipant;
 import sejong.teemo.ingamesearch.dto.Spectator;
 import sejong.teemo.ingamesearch.facade.annotation.Facade;
 import sejong.teemo.ingamesearch.service.SpectatorService;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Facade
 @RequiredArgsConstructor
@@ -12,9 +17,21 @@ public class SpectatorFacade {
 
     private final SpectatorService spectatorService;
 
-    public Spectator callSpectator(String gameName, String tag) {
+    public List<ChampionMastery> callSpectator(String gameName, String tag) {
 
         Account account = spectatorService.callRiotPUUID(gameName, tag);
-        return spectatorService.callRiotSpectatorV5(account.puuid());
+        Spectator spectator = spectatorService.callRiotSpectatorV5(account.puuid());
+
+        return asyncCallChampionMastery(spectator.currentGameParticipants());
+    }
+
+    private List<ChampionMastery> asyncCallChampionMastery(List<CurrentGameParticipant> currentGameParticipants) {
+
+        List<CompletableFuture<ChampionMastery>> futures = currentGameParticipants.stream()
+                .map(currentGameParticipant -> CompletableFuture.supplyAsync(() ->
+                                spectatorService.callRiotChampionMastery(currentGameParticipant.puuid(), currentGameParticipant.championId())
+                        )).toList();
+
+        return futures.stream().map(CompletableFuture::join).toList();
     }
 }
