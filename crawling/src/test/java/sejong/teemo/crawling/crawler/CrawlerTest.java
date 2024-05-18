3c7f0962346fb8f1f82ed3c2737c380f972adf0b
swager253 <swager253@naver.com>
2024-05-18 15:39:18 +0900

    test: 순위 정보를 비동기 크롤링 하는 테스트

--- crawling/src/test/java/sejong/teemo/crawling/crawler/CrawlerTest.java
+++ crawling/src/test/java/sejong/teemo/crawling/crawler/CrawlerTest.java
@@ -6,36 +6,45 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.TestPropertySources;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import sejong.teemo.crawling.CrawlingApplication;
+import sejong.teemo.crawling.container.TestContainer;
+import sejong.teemo.crawling.crawler.page.LeaderBoardPage;
 import sejong.teemo.crawling.domain.Summoner;
 import sejong.teemo.crawling.dto.MatchDataDto;
 import sejong.teemo.crawling.mapper.CrawlerMapperMatchData;
 import sejong.teemo.crawling.mapper.CrawlerMapperSummoner;
+import sejong.teemo.crawling.property.CrawlingProperties;
 import sejong.teemo.crawling.property.CrawlingPropertiesV1;
-import sejong.teemo.crawling.util.ParserUtil;
-import sejong.teemo.crawling.webDriver.generator.UrlGenerator;
+import sejong.teemo.crawling.webDriver.pool.WebDriverPool;
+import sejong.teemo.crawling.webDriver.pool.WebDriverPoolingFactory;
 
-import java.net.URI;
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.*;
+import static sejong.teemo.crawling.webDriver.generator.UrlGenerator.*;
 
-class CrawlerTest {
+class CrawlerTest extends TestContainer {
+
+    @Autowired
+    private CrawlingPropertiesV1 crawlingPropertiesV1;
 
     @Test
     void 매치_데이터를_크롤링_하고_가져와서_정제하여_리스트를_반환한다() {
@@ -46,10 +55,10 @@ class CrawlerTest {
         String tag = "KR1";
 
         // when
-        List<MatchDataDto> dtoList = PageCrawler.<MatchDataDto>builder()
+        List<MatchDataDto> dtoList = Crawler.<MatchDataDto>builder()
                 .driver(webDriver)
                 .driverWait(new WebDriverWait(webDriver, Duration.ofSeconds(10)))
-                .urlGenerator(UrlGenerator.RIOT_SUMMONERS)
+                .urlGenerator(RIOT_SUMMONERS)
                 .build()
                 .urlGenerate(urlGenerator -> urlGenerator.generateUrl(name + "-" + tag))
                 .click(By.cssSelector("li.css-1j5gzz7:nth-child(2)"))
@@ -72,10 +81,10 @@ class CrawlerTest {
         WebDriver webDriver = new FirefoxDriver();
 
         // when
-        List<Summoner> summoners = PageCrawler.<Summoner>builder()
+        List<Summoner> summoners = Crawler.<Summoner>builder()
                 .driver(webDriver)
                 .driverWait(new WebDriverWait(webDriver, Duration.ofSeconds(20)))
-                .urlGenerator(UrlGenerator.RIOT_LEADER_BOARD)
+                .urlGenerator(RIOT_LEADER_BOARD)
                 .build()
                 .urlGenerate(urlGenerator -> urlGenerator.generateUrl(1))
                 .isWaitingUntilLoadedPage(By.cssSelector("tbody"))
@@ -89,5 +98,29 @@ class CrawlerTest {
         // then
         webDriver.close();
         assertThat(summoners).hasSize(100);
+    }
+
+    @Test
+    void 여러개의_순위_정보를_비동기_크롤링_하고_가져와서_정제하여_리스트를_반환한다() {
+        // given
+        int maxPoolSize = 2;
+        int startPage = 1, endPage = 5;
+        WebDriverPool webDriverPool = new WebDriverPool(new WebDriverPoolingFactory(crawlingPropertiesV1), maxPoolSize);
+        ExecutorService executorService = Executors.newFixedThreadPool(maxPoolSize);
+
+        // when
+        List<List<Summoner>> list = AsyncCrawler.<Summoner>builder()
+                .pool(webDriverPool)
+                .executor(executorService)
+                .build()
+                .setPages(startPage, endPage)
+                .poolSize(maxPoolSize)
+                .action((webDriver, pageNum) -> new LeaderBoardPage().crawler(webDriver, RIOT_LEADER_BOARD, String.valueOf(pageNum)));
+
+        executorService.shutdown();
+        webDriverPool.close();
+
+        // then
+        assertThat(list).hasSize(5);
     }
 }
