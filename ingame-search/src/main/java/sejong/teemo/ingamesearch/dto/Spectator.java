c9507615d2064034dbff7528778f8458ab052d5c
swager253 <swager253@naver.com>
2024-05-18 22:26:51 +0900

    rename: 필드 명 변경

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/dto/Spectator.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/dto/Spectator.java
@@ -5,5 +5,5 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Spectator(String gameType, List<CurrentGameParticipant> currentGameParticipants) {
+public record Spectator(String gameType, List<CurrentGameParticipant> participants) {
 }
