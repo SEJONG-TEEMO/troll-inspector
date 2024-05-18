9ed4c3ba0fee2a5f316ccf299bcf5e2fd0e9264c
swager253 <swager253@naver.com>
2024-05-18 15:38:32 +0900

    feat: api key 쿼리스트링 -> 헤더로 변경하기 위한 header key 값 수정

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/service/SpectatorService.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/service/SpectatorService.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import sejong.teemo.ingamesearch.dto.Account;
+import sejong.teemo.ingamesearch.dto.ChampionMastery;
 import sejong.teemo.ingamesearch.dto.Spectator;
 import sejong.teemo.ingamesearch.exception.ExceptionProvider;
 import sejong.teemo.ingamesearch.exception.FailedApiCallingException;
@@ -22,16 +23,16 @@ public class SpectatorService {
     private final RestClient restClient;
     private final ApikeyProperties apikeyProperties;
 
-    private static final String API_KEY = "api_key";
+    private static final String API_KEY = "X-Riot-Token";
 
     public Spectator callRiotSpectatorV5(String puuid) {
 
         return restClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path(ApiUrlGenerator.RIOT_SPECTATOR.getUrl())
-                        .queryParam(API_KEY, apikeyProperties.apiKey())
                         .build(puuid))
                 .accept(APPLICATION_JSON)
+                .header(API_KEY, apikeyProperties.apiKey())
                 .retrieve()
                 .onStatus(HttpStatusCode::is4xxClientError, ((request, response) -> {
                     log.error("spectator error status = {} message = {}", response.getStatusCode(), response.getStatusText());
@@ -42,17 +43,35 @@ public class SpectatorService {
 
     public Account callRiotPUUID(String gameName, String tag) {
 
+        log.info("api key = {}", apikeyProperties.apiKey());
+
         return restClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path(ApiUrlGenerator.RIOT_ACCOUNT.getUrl())
-                        .queryParam(API_KEY, apikeyProperties.apiKey())
                         .build(gameName, tag))
                 .accept(APPLICATION_JSON)
+                .header(API_KEY, apikeyProperties.apiKey())
                 .retrieve()
                 .onStatus(HttpStatusCode::is4xxClientError, ((request, response) -> {
-                    log.error("account error status = {} message = {}", response.getStatusCode(), response.getStatusText());
+                    log.error("account error status = {}, message = {}", response.getStatusCode(), response.getStatusText());
                     throw new FailedApiCallingException(ExceptionProvider.RIOT_ACCOUNT_API_CALL_FAILED);
                 }))
                 .body(Account.class);
+    }
+
+    public ChampionMastery callRiotChampionMastery(String puuid, Long championId) {
+
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(ApiUrlGenerator.RIOT_CHAMPION_MASTERY.getUrl())
+                        .build(puuid, championId))
+                .accept(APPLICATION_JSON)
+                .header(API_KEY, apikeyProperties.apiKey())
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, ((request, response) -> {
+                    log.error("champion mastery error status = {}, message = {}", response.getStatusCode(), response.getStatusText());
+                    throw new FailedApiCallingException(ExceptionProvider.RIOT_ACCOUNT_API_CALL_FAILED);
+                }))
+                .body(ChampionMastery.class);
     }
 }
