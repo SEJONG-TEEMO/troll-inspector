b78f0196a5f4f62ebf6ac925d3e7a32688781225
swager253 <swager253@naver.com>
2024-05-20 21:39:44 +0900

    feat: 배치 설정 추가

---
+++ crawling/src/main/java/sejong/teemo/crawling/batch/config/CrawlingBatchConfig.java
@@ -0,0 +1,9 @@
+package sejong.teemo.crawling.batch.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class CrawlingBatchConfig {
+}
