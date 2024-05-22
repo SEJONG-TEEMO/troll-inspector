5f5adef0966536dab763602e1d5070f9ba47b9f9
gkdbssla97 <gkdbssla97@github.com>
2024-05-22 15:22:55 +0900

    feat(ElasticSearchConfig): ElasticSearchConfig 설정값 Config

---
+++ troll-inspector/src/main/java/sejong/teemo/trollinspector/config/ElasticSearchConfig.java
@@ -0,0 +1,18 @@
+package sejong.teemo.trollinspector.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "sejong.teemo.trollinspector.repository")
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+    }
+}
