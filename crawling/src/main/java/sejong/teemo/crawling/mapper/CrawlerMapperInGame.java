b28d53db179e13595a87f51e76e9bc868d588e3a
swager253 <swager253@naver.com>
2024-05-24 23:10:22 +0900

    refactor: 크롤링 인게임 DTO 스펙 변경으로 인한 리팩토링

--- crawling/src/main/java/sejong/teemo/crawling/mapper/CrawlerMapperInGame.java
+++ crawling/src/main/java/sejong/teemo/crawling/mapper/CrawlerMapperInGame.java
@@ -9,6 +9,7 @@ public class CrawlerMapperInGame implements CrawlerMapper<InGameDto> {
         String[] split = crawlingData.split("\n");
 
         InGameDto.InGameDtoBuilder inGameDtoBuilder = InGameDto.builder()
+                .championId(0L)
                 .gameName(split[0])
                 .level(Integer.parseInt(split[1].split(" ")[1]))
                 .tier(split[2])
@@ -17,7 +18,7 @@ public class CrawlerMapperInGame implements CrawlerMapper<InGameDto> {
         if(split.length == 9) {
             inGameDtoBuilder
                     .rateOfChampionWin(split[4])
-                    .countChampion(Integer.parseInt(split[5].split(" ")[0]))
+                    .countChampion(Integer.parseInt(split[5].replaceAll("[()]", "").split(" ")[0]))
                     .totalKDA(split[6])
                     .KDA(split[7]);
         } else {
