4f04e4076f7007c71c03a54a797b5dbd585133c4
swager253 <swager253@naver.com>
2024-05-24 23:10:56 +0900

    feat: cors 정책 추가

---
+++ crawling/src/main/java/sejong/teemo/crawling/config/WebConfig.java
@@ -0,0 +1,18 @@
+package sejong.teemo.crawling.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*");
+    }
+}
