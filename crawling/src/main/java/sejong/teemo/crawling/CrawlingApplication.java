b204f2d9cf39f6574760437bd649a5b82de01e84
swager253 <swager253@naver.com>
2024-05-28 21:30:48 +0900

    refactor: DataSourceAutoConfiguration exclude

--- crawling/src/main/java/sejong/teemo/crawling/CrawlingApplication.java
+++ crawling/src/main/java/sejong/teemo/crawling/CrawlingApplication.java
@@ -1,14 +1,12 @@
 package sejong.teemo.crawling;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableBatchProcessing
 @ConfigurationPropertiesScan
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CrawlingApplication {
 
     public static void main(String[] args) {
