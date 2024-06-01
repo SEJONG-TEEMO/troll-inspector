5871d71033254b45ac558ae3a779478842520d41
swager253 <swager253@naver.com>
2024-06-01 19:05:04 +0900

    feat: riot api 모듈 spectator api 추가

--- riot-api/src/main/java/sejong/teemo/riotapi/api/v1/SpectatorApiV1.java
+++ riot-api/src/main/java/sejong/teemo/riotapi/api/v1/SpectatorApiV1.java
@@ -1,7 +1,16 @@
 package sejong.teemo.riotapi.api.v1;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sejong.teemo.riotapi.service.SpectatorService;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/teemo.gg/api/v1")
 public class SpectatorApiV1 {
+
+    private final SpectatorService spectatorService;
+
+
 }
