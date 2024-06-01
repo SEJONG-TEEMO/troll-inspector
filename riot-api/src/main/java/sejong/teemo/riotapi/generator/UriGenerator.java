f1f5e7df32e9ff6a460fb31c18a185213e4577d6
swager253 <swager253@naver.com>
2024-06-01 19:02:58 +0900

    rename: ApiUrlGenerator -> UriGenerator 이름 변경

--- riot-api/src/main/java/sejong/teemo/riotapi/generator/UriGenerator.java
+++ riot-api/src/main/java/sejong/teemo/riotapi/generator/UriGenerator.java
@@ -6,7 +6,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 
 @Getter
-public enum ApiUrlGenerator {
+public enum UriGenerator {
 
     RIOT_SPECTATOR("https://kr.api.riotgames.com/lol/spectator/v5/active-games/by-summoner/{encryptedPUUID}"),
     RIOT_ACCOUNT("https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{gameName}/{tag}"),
@@ -16,12 +16,16 @@ public enum ApiUrlGenerator {
 
     private final String url;
 
-    ApiUrlGenerator(String url) {
+    UriGenerator(String url) {
         this.url = url;
     }
 
     public URI generateUri(Object... obj) {
         return UriComponentsBuilder.fromHttpUrl(this.url).build(obj);
+    }
+
+    public URI generateUri(int page, Object... obj) {
+        return UriComponentsBuilder.fromHttpUrl(this.url).queryParam("page", page).build(obj);
     }
 
     public String skipString(String target, String str) {
