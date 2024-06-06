298e7e11fcf50131eee65e2949d8e6209c9d86e8
swager253 <swager253@naver.com>
2024-06-06 16:07:49 +0900

    test: riot api test

---
+++ riot-api/src/test/java/sejong/teemo/riotapi/api/v1/LeagueApiV1Test.java
@@ -0,0 +1,47 @@
+package sejong.teemo.riotapi.api.v1;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import sejong.teemo.riotapi.facade.UserInfoFacade;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class LeagueApiV1Test {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserInfoFacade userInfoFacade;
+
+    @Test
+    void 리그_소환사_API_응답_테스트() throws Exception {
+        // given
+        String division = "I";
+        String tier = "DIAMOND";
+        String queue = "RANKED_SOLO_5x5";
+
+        int page = 1;
+
+        // when
+        ResultActions resultActions = mockMvc.perform(get("/teemo.gg/api/v1/league-to-summoner/{division}/{tier}/{queue}", division, tier, queue)
+                .param("page", String.valueOf(page))
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        resultActions.andExpect(status().isOk())
+                .andDo(print());
+    }
+}
---
+++ riot-api/src/test/java/sejong/teemo/riotapi/extension/TestExtension.java
@@ -0,0 +1,92 @@
+package sejong.teemo.riotapi.extension;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.test.context.ActiveProfiles;
+import sejong.teemo.riotapi.dto.*;
+import sejong.teemo.riotapi.properties.RiotApiProperties;
+
+import java.util.List;
+
+@EnableConfigurationProperties(RiotApiProperties.class)
+@ActiveProfiles("test")
+@RestClientTest
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class TestExtension {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    protected String getAccount() {
+        try {
+            return objectMapper.writeValueAsString(new Account("PUUID", "a1h", "KR1"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected String getSpectator() {
+        try {
+            return objectMapper.writeValueAsString(new Spectator("type", List.of()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected String getLeague() {
+
+        MiniSeriesDto miniSeries = MiniSeriesDto.builder()
+                .losses(1)
+                .progress("WLLWN")
+                .target(3)
+                .wins(2)
+                .build();
+
+        LeagueEntryDto leagueEntry = LeagueEntryDto.builder()
+                .leagueId("league123")
+                .summonerId("summoner456")
+                .queueType("RANKED_SOLO_5x5")
+                .tier("DIAMOND")
+                .rank("I")
+                .leaguePoints(100)
+                .wins(20)
+                .losses(10)
+                .hotStreak(true)
+                .veteran(false)
+                .freshBlood(true)
+                .inactive(false)
+                .miniSeriesDto(miniSeries)
+                .build();
+
+        List<LeagueEntryDto> leagueEntryDtos = List.of(leagueEntry);
+
+        try {
+            return objectMapper.writeValueAsString(leagueEntryDtos);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected String getSummoner() {
+
+        SummonerDto summoner = SummonerDto.builder()
+                .summonerId("summoner789")
+                .accountId("account123")
+                .profileIconId(1234)
+                .revisionDate(System.currentTimeMillis())
+                .puuid("puuid-1234-5678-91011-1213")
+                .summonerLevel(30)
+                .build();
+
+        try {
+            return objectMapper.writeValueAsString(summoner);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
