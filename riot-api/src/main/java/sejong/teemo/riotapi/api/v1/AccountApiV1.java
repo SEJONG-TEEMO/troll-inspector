e3ba91489e6a27862de74ceae094f5f0e9b52311
swager253 <swager253@naver.com>
2024-06-08 16:49:35 +0900

    remove: import 제거 UserInfoFacade

--- riot-api/src/main/java/sejong/teemo/riotapi/api/v1/AccountApiV1.java
+++ riot-api/src/main/java/sejong/teemo/riotapi/api/v1/AccountApiV1.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sejong.teemo.riotapi.dto.Account;
-import sejong.teemo.riotapi.facade.UserInfoFacade;
 import sejong.teemo.riotapi.service.AccountService;
 
 @RestController
