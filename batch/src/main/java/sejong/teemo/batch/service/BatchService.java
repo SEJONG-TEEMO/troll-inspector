88f4f9ab79e963f422f525eb6aa1f6e3477436b4
swager253 <swager253@naver.com>
2024-06-11 23:56:43 +0900

    feat: BatchService retry 추가

--- batch/src/main/java/sejong/teemo/batch/service/BatchService.java
+++ batch/src/main/java/sejong/teemo/batch/service/BatchService.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import sejong.teemo.batch.dto.UserInfoDto;
@@ -22,6 +25,12 @@ public class BatchService {
 
     private final RestClient restClient;
 
+    @Retryable(
+            retryFor = FailedApiCallingException.class,
+            backoff = @Backoff(delay = 10000),
+            recover = "returnEmptyList",
+            maxAttempts = 5
+    )
     public List<UserInfoDto> callApiUserInfo(String division, String tier, String queue, int page) {
 
         return restClient.get()
@@ -31,8 +40,13 @@ public class BatchService {
                 .onStatus(HttpStatusCode::is4xxClientError, ((request, response) -> {
                     log.info("user info = {}", request.getURI());
                     log.error("user info api call error!! = {}", response.getStatusText());
-                    throw  new FailedApiCallingException(ExceptionProvider.RIOT_API_MODULE_USER_INFO_FAILED);
+                    throw new FailedApiCallingException(ExceptionProvider.RIOT_API_MODULE_USER_INFO_FAILED);
                 })).body(new ParameterizedTypeReference<>() {});
     }
 
+    @Recover
+    public List<UserInfoDto> returnEmptyList(FailedApiCallingException e, String division, String tier, String queue, int page) {
+        log.info("recover method execute = {}", e.getMessage());
+        return List.of();
+    }
 }
