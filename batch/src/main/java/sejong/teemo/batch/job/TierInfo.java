86bc8f2e42fa52b769aabd857951f6c5707749d7
swager253 <swager253@naver.com>
2024-06-11 23:52:41 +0900

    refactor: tierInfo 상수 값 String -> int 로 변경, mapToString 메서드 추가

--- batch/src/main/java/sejong/teemo/batch/job/TierInfo.java
+++ batch/src/main/java/sejong/teemo/batch/job/TierInfo.java
@@ -4,19 +4,35 @@ import lombok.Getter;
 
 @Getter
 public enum TierInfo {
-    IRON("IRON"),
-    SILVER("SILVER"),
-    GOLD("GOLD"),
-    PLATINUM("PLATINUM"),
-    EMERALD("EMERALD"),
-    DIAMOND("DIAMOND"),
-    MASTER("MASTER"),
-    GRAND_MASTER("GRANDMASTER"),
-    CHALLENGER("CHALLENGER");
+    IRON(1),
+    BRONZE(2),
+    SILVER(3),
+    GOLD(4),
+    PLATINUM(5),
+    EMERALD(6),
+    DIAMOND(7),
+    MASTER(8),
+    GRAND_MASTER(9),
+    CHALLENGER(10);
 
-    private final String value;
+    private final int value;
 
-    TierInfo(String value) {
+    TierInfo(int value) {
         this.value = value;
+    }
+
+    public String mapToString() {
+        return switch (this) {
+            case IRON -> "IRON";
+            case BRONZE -> "BRONZE";
+            case SILVER -> "SILVER";
+            case GOLD -> "GOLD";
+            case PLATINUM -> "PLATINUM";
+            case EMERALD -> "EMERALD";
+            case DIAMOND -> "DIAMOND";
+            case MASTER -> "MASTER";
+            case GRAND_MASTER -> "GRANDMASTER";
+            case CHALLENGER -> "CHALLENGER";
+        };
     }
 }
