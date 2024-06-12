0defdb30e2aca8198e4fd1a2f16263a312b9fe27
gkdbssla97 <gkdbssla97@github.com>
2024-06-12 19:00:08 +0900

    feat(MatchController): puuid 인자로 트롤-분석API 호출 로직 추가

--- troll-inspector/src/main/java/sejong/teemo/trollinspector/controller/MatchController.java
+++ troll-inspector/src/main/java/sejong/teemo/trollinspector/controller/MatchController.java
@@ -7,25 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sejong.teemo.trollinspector.domain.SummonerPerformance;
+import reactor.core.publisher.Mono;
 import sejong.teemo.trollinspector.service.MatchService;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/matches")
+@RequestMapping("/api/troll-inspector/v1")
 @Slf4j
 public class MatchController {
 
     private final MatchService matchService;
 
     @GetMapping("/ids/{gameName}/{tagLine}")
-    public ResponseEntity<List<SummonerPerformance>> analyzeSummonerPerformance(@PathVariable("gameName") String gameName,
-                                                                                @PathVariable("tagLine") String tagLine) throws Exception {
-        List<SummonerPerformance> matchIds = matchService.analyzeSummonerPerformanceForPersonalKey(gameName, tagLine);
+    public Mono<ResponseEntity<Void>> analyzeSummonerPerformance(@PathVariable("gameName") String gameName,
+                                                                 @PathVariable("tagLine") String tagLine) {
+        return matchService.analyzeSummonerPerformance(gameName, tagLine)
+                .then(Mono.just(ResponseEntity.ok().build()));
+    }
 
-        return ResponseEntity.ok(matchIds);
+    @GetMapping("/ids/{puuid}")
+    public Mono<ResponseEntity<Void>> analyzeSummonerPerformance(@PathVariable("puuid") String puuid) {
+        return matchService.analyzeSummonerPerformance(puuid)
+                .then(Mono.just(ResponseEntity.ok().build()));
     }
 }
 
