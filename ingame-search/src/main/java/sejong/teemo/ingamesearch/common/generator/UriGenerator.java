c93f0dd8e8602d5816bc4d9e0631bce59fbebcc7
swager253 <swager253@naver.com>
2024-06-13 15:49:45 +0900

    feat: SummonerPerformance API 호출을 위한 uri 생성

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/common/generator/UriGenerator.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/common/generator/UriGenerator.java
@@ -14,7 +14,9 @@ public enum UriGenerator {
     RIOT_API_SUMMONER("http://localhost:8080/teemo.gg/api/v1//summoner/{encryptedSummonerId}"),
     RIOT_API_ACCOUNT("http://localhost:8080/teemo.gg/api/v1/account/{encryptedPuuid}"),
     RIOT_API_USER_INFO("http://localhost:8080/teemo.gg/api/v1/user-info/{division}/{tier}/{queue}"),
-    RIOT_API_SPECTATOR("http://localhost:8080/teemo.gg/api/v1/spectator/{gameName}/{tag}");
+    RIOT_API_SPECTATOR("http://localhost:8080/teemo.gg/api/v1/spectator/{gameName}/{tag}"),
+    RIOT_API_SUMMONER_PERFORMANCE_GAME_NAME_TAG("http://localhost:8080/teemo.gg/api/v1/match/summoner-performance/{gameName}/{tag}"),
+    RIOT_API_SUMMONER_PERFORMANCE_PUUID("http://localhost:8080/teemo.gg/api/v1/match/summoner-performance/{puuid}");
 
     private final String url;
 
