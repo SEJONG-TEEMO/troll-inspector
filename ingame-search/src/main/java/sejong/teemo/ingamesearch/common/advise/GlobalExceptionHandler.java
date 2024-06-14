b47411d4c16fbc61e9e91b12d0dd02fafb5f3a3f
swager253 <swager253@naver.com>
2024-06-14 17:39:09 +0900

    rename: 공통 기능을 활성화 하기 위해 common 패키지로 이동

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/common/advise/GlobalExceptionHandler.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/common/advise/GlobalExceptionHandler.java
@@ -1,14 +1,15 @@
-package sejong.teemo.ingamesearch.ingame.api.advise;
+package sejong.teemo.ingamesearch.common.advise;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import sejong.teemo.ingamesearch.ingame.exception.FailedApiCallingException;
+import sejong.teemo.ingamesearch.common.exception.FailedApiCallingException;
 
 @Slf4j
 @RestControllerAdvice(annotations = RestController.class)
@@ -18,5 +19,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     protected ErrorResponse handleFailedApiCallingException(FailedApiCallingException e) {
         return e.getExceptionProvider();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ErrorResponse handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ErrorResponseException(HttpStatus.BAD_REQUEST, e);
     }
 }
