7405068c1c83b0e59bb022385bf38da190cf8082
swager253 <swager253@naver.com>
2024-06-15 15:39:53 +0900

    test: test container dynamic property 부분 삭제 yml로 처리하도록 구성함

--- batch/src/test/java/sejong/teemo/batch/container/TestContainer.java
+++ batch/src/test/java/sejong/teemo/batch/container/TestContainer.java
@@ -3,8 +3,6 @@ package sejong.teemo.batch.container;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -17,21 +15,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class TestContainer {
 
-    private static final String MYSQL_DB = "mysqldb";
-    private static final int MY_SQL_PORT = 3306;
     private static final String MYSQL_VERSION = "mysql:8.2.0";
 
     @Container
-    private static final MySQLContainer mySqlContainer = new MySQLContainer(MYSQL_VERSION);
+    private static final MySQLContainer<?> mySqlContainer = new MySQLContainer<>(MYSQL_VERSION);
 
     static {
-        mySqlContainer.withDatabaseName(MYSQL_DB);
-        mySqlContainer.withInitScript("schema-mysql.sql");
-    }
-
-    @DynamicPropertySource
-    static void dynamicProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.username", mySqlContainer::getUsername);
-        registry.add("spring.datasource.password", mySqlContainer::getPassword);
+        mySqlContainer.withUsername("test");
+        mySqlContainer.withPassword("test");
     }
 }
