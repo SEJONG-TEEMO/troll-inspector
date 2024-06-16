0627523e59e73255ca49d7c3f4ae429244b7cccd
swager253 <swager253@naver.com>
2024-06-16 18:13:54 +0900

    feat: 429 응답에 대한 커스텀 예외 클래스 작성

---
+++ batch/src/main/java/sejong/teemo/batch/exception/TooManyApiCallingException.java
@@ -0,0 +1,34 @@
+package sejong.teemo.batch.exception;
+
+import lombok.Getter;
+
+@Getter
+public class TooManyApiCallingException extends RuntimeException {
+
+    private final ExceptionProvider exceptionProvider;
+
+    public TooManyApiCallingException(ExceptionProvider exceptionProvider) {
+        super(exceptionProvider.getMessage());
+        this.exceptionProvider = exceptionProvider;
+    }
+
+    public TooManyApiCallingException(String message, ExceptionProvider exceptionProvider) {
+        super(message);
+        this.exceptionProvider = exceptionProvider;
+    }
+
+    public TooManyApiCallingException(String message, Throwable cause, ExceptionProvider exceptionProvider) {
+        super(message, cause);
+        this.exceptionProvider = exceptionProvider;
+    }
+
+    public TooManyApiCallingException(Throwable cause, ExceptionProvider exceptionProvider) {
+        super(cause);
+        this.exceptionProvider = exceptionProvider;
+    }
+
+    public TooManyApiCallingException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, ExceptionProvider exceptionProvider) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.exceptionProvider = exceptionProvider;
+    }
+}
