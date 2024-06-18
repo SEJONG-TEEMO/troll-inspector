ffe69acc203ce64a1e9d4d2c7a9199904ea07373
swager253 <swager253@naver.com>
2024-06-18 22:58:30 +0900

    feat: 가짜 데이터 생성하는 메서드 추가

--- batch/src/test/java/sejong/teemo/batch/container/TestContainer.java
+++ batch/src/test/java/sejong/teemo/batch/container/TestContainer.java
@@ -1,11 +1,17 @@
 package sejong.teemo.batch.container;
 
+import net.datafaker.Faker;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import sejong.teemo.batch.entity.UserInfo;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,5 +29,29 @@ public class TestContainer {
     static {
         mySqlContainer.withUsername("test");
         mySqlContainer.withPassword("test");
+    }
+
+    protected List<UserInfo> dataInit() {
+
+        Faker faker = new Faker();
+
+        return IntStream.range(0, 100)
+                .mapToObj(i -> UserInfo.of(
+                        faker.gameOfThrones().character(),
+                        faker.lorem().word(),
+                        faker.internet().uuid(),
+                        faker.internet().uuid(),
+                        faker.options().option("Solo", "Duo", "Flex"),
+                        faker.options().option("IRON", "BRONZE", "SILVER", "GOLD", "PLATINUM", "EMERALD", "DIAMOND", "MASTER", "GRAND_MASTER", "CHALLENGER"),
+                        faker.options().option("I", "II", "III", "IV"),
+                        faker.number().numberBetween(0, 500),
+                        faker.number().numberBetween(0, 500),
+                        faker.number().numberBetween(0, 1000),
+                        faker.internet().uuid(),
+                        faker.number().numberBetween(0, 1000),
+                        ThreadLocalRandom.current().nextLong(1000000000L, 2000000000L),
+                        faker.number().numberBetween(1, 500)
+                ))
+                .toList();
     }
 }
