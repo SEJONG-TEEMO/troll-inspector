d42a1db30501915abd7c5793255215fe34e8ed74
swager253 <swager253@naver.com>
2024-06-20 23:48:10 +0900

    remove: 불필요한 클래스 삭제

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/common/property/ApikeyProperties.java
+++
@@ -1,7 +0,0 @@
-package sejong.teemo.ingamesearch.common.property;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "riot")
-public record ApikeyProperties(String apiKey) {
-}
