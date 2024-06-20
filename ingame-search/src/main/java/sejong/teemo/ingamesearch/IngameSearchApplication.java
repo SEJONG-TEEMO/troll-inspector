997be91d40dd8898815a49eed4eee8ba66111496
swager253 <swager253@naver.com>
2024-06-21 00:13:00 +0900

    remove: @ConfigurationPropertiesScan 삭제

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/IngameSearchApplication.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/IngameSearchApplication.java
@@ -2,10 +2,7 @@ package sejong.teemo.ingamesearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
 public class IngameSearchApplication {
 
