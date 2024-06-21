22712614899eb6c2a5ab95f5d4e59ed5422c8a25
swager253 <swager253@naver.com>
2024-06-21 14:56:07 +0900

    refactor: ingame api request param으로 변경

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/api/v1/InGameApiV1.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/api/v1/InGameApiV1.java
@@ -4,16 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sejong.teemo.ingamesearch.ingame.facade.SummonerPerformanceFacade;
+import sejong.teemo.ingamesearch.ingame.dto.InGameView;
+import sejong.teemo.ingamesearch.ingame.facade.InGameFacade;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class InGameApiV1 {
 
-    private final SummonerPerformanceFacade summonerPerformanceFacade;
+    private final InGameFacade inGameFacade;
 
-    @GetMapping("/in-game/{gameName}/{tagLine}")
-    public ResponseEntity<>
+    @GetMapping("/in-game")
+    public ResponseEntity<List<InGameView>> inGameApi(@RequestParam("gameName") String gameName,
+                                                      @RequestParam("tagLine") String tagLine) {
+
+        return ResponseEntity.ok(inGameFacade.inGame(gameName, tagLine));
+    }
 }
