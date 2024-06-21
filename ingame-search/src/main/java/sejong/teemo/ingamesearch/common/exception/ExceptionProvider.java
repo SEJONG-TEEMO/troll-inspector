be74b9c921af103f5e15b8ebc26b3512bdf05bcf
swager253 <swager253@naver.com>
2024-06-21 14:51:47 +0900

    feat: 예외 400번대, 500번대 처리 추가

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/common/exception/ExceptionProvider.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/common/exception/ExceptionProvider.java
@@ -1,18 +1,38 @@
 package sejong.teemo.ingamesearch.common.exception;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.ErrorResponse;
 
+import javax.naming.ServiceUnavailableException;
+import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.TOO_MANY_REQUESTS;
+
 @Getter
+@Slf4j
 public enum ExceptionProvider implements ErrorResponse {
 
-    RIOT_SPECTATOR_API_CALL_FAILED(HttpStatus.NOT_FOUND, "SPECTATOR_100","SPECTATOR API 호출에 실패하였습니다."),
-    RIOT_ACCOUNT_API_CALL_FAILED(HttpStatus.NOT_FOUND, "ACCOUNT_100", "ACCOUNT API 호출에 실패하였습니다."),
-    RIOT_CHAMPION_MASTERY_API_CALL_FAILED(HttpStatus.NOT_FOUND, "CHAMPION_MASTERY_100", "CHAMPION_MASTERY API 호출에 실패하였습니다."),
-    RIOT_SUMMONER_PERFORMANCE_API_CALL_FAILED(HttpStatus.NOT_FOUND, "SUMMONER_PERFORMANCE_100", "SUMMONER PERFORMANCE API 호출에 실패하였습니다.");
+    // 400
+    RIOT_SPECTATOR_API_CALL_FAILED(NOT_FOUND, "SPECTATOR_100","SPECTATOR API 호출에 실패하였습니다."),
+    RIOT_ACCOUNT_API_CALL_FAILED(NOT_FOUND, "ACCOUNT_100", "ACCOUNT API 호출에 실패하였습니다."),
+    RIOT_CHAMPION_MASTERY_API_CALL_FAILED(NOT_FOUND, "CHAMPION_MASTERY_100", "CHAMPION_MASTERY API 호출에 실패하였습니다."),
+    RIOT_SUMMONER_PERFORMANCE_API_CALL_FAILED(NOT_FOUND, "SUMMONER_PERFORMANCE_100", "SUMMONER PERFORMANCE API 호출에 실패하였습니다."),
+    RIOT_USER_INFO_API_CALL_FAILED(NOT_FOUND, "USER_INFO_100", "USER INFO API 호출에 실패하였습니다."),
+    RIOT_LEAGUE_ENTRY_API_CALL_FAILED(NOT_FOUND, "LEAGUE_ENTRY_100", "LEAGUE_ENTRY API 호출에 실패하였습니다."),
+    RIOT_SUMMONER_API_CALL_FAILED(NOT_FOUND, "SUMMONER_100", "SUMMONER API 호출에 실패하였습니다."),
+    RIOT_CHAMPION_IMAGE(NOT_FOUND, "CHAMPION_IMAGE_100", "CHAMPION_IMAGE 를 찾을 수 없습니다."),
+    TOO_MANY_CALLING_FAILED(TOO_MANY_REQUESTS, "TOO_MANY_100", "riot api 에 과도하게 요청되었습니다."),
+
+    // 500
+    RIOT_INTERNAL_SERVER_ERROR_FAILED(INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_100", "내부 서버의 오류가 발생하였습니다."),
+    RIOT_SERVICE_AVAILABLE_FAILED(SERVICE_UNAVAILABLE, "SERVICE_AVAILABLE_100", "서비스를 이용할 수 없습니다. (Critical!!)");
 
     private final HttpStatus httpStatus;
     private final String code;
@@ -22,6 +42,31 @@ public enum ExceptionProvider implements ErrorResponse {
         this.code = code;
         this.httpStatus = httpStatus;
         this.message = message;
+    }
+
+    public void handler(HttpRequest request, ClientHttpResponse response) {
+        try {
+            log.info("get uri = {}", request.getURI());
+            log.error("account error status = {} message = {} header = {}",
+                    response.getStatusCode(),
+                    response.getStatusText(),
+                    response.getHeaders());
+
+            exception(response);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private void exception(ClientHttpResponse response) throws IOException {
+        switch (response.getStatusCode()) {
+            case NOT_FOUND, FORBIDDEN -> throw new FailedApiCallingException(this);
+            case TOO_MANY_REQUESTS -> throw new TooManyApiCallingException(this);
+            case BAD_REQUEST -> throw new IllegalArgumentException("riot api 모듈 요청에 실패하였습니다.");
+            case INTERNAL_SERVER_ERROR, SERVICE_UNAVAILABLE -> throw new RuntimeException(this.getMessage());
+            default -> throw new IllegalStateException("Unexpected value: " + response.getStatusCode());
+        }
     }
 
     @Override
