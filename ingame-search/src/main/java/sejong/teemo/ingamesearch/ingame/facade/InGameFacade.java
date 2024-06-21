87eb5ae4027bd48399377076357c045b59b315cf
swager253 <swager253@naver.com>
2024-06-21 14:56:48 +0900

    refactor: InGameFacade를 추가 하여 여러 서비스를 추상화시킴

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/facade/InGameFacade.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/facade/InGameFacade.java
@@ -2,29 +2,93 @@ package sejong.teemo.ingamesearch.ingame.facade;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import sejong.teemo.ingamesearch.ingame.async.AsyncCall;
-import sejong.teemo.ingamesearch.dto.*;
-import sejong.teemo.ingamesearch.ingame.dto.ChampionMastery;
+import sejong.teemo.ingamesearch.champion.service.ChampionService;
+import sejong.teemo.ingamesearch.common.async.AsyncCall;
 import sejong.teemo.ingamesearch.ingame.dto.InGameView;
-import sejong.teemo.ingamesearch.ingame.service.SpectatorService;
+import sejong.teemo.ingamesearch.ingame.dto.SpectatorDto;
+import sejong.teemo.ingamesearch.ingame.dto.champion.Champion;
+import sejong.teemo.ingamesearch.ingame.dto.champion.ChampionMastery;
+import sejong.teemo.ingamesearch.ingame.dto.summoner.SummonerPerformance;
+import sejong.teemo.ingamesearch.ingame.dto.user.Account;
+import sejong.teemo.ingamesearch.ingame.dto.user.LeagueEntryDto;
+import sejong.teemo.ingamesearch.ingame.dto.user.SummonerDto;
+import sejong.teemo.ingamesearch.ingame.dto.user.UserInfoDto;
+import sejong.teemo.ingamesearch.ingame.repository.InGameRepository;
+import sejong.teemo.ingamesearch.ingame.service.InGameService;
 
 import java.util.List;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
 public class InGameFacade {
 
-    private final SpectatorService spectatorService;
+    private final InGameService inGameService;
+    private final ChampionService championService;
+    private final InGameRepository inGameRepository;
 
     public List<InGameView> inGame(String gameName, String tagLine) {
-        return null;
+
+        String checkPuuid = getPuuid(gameName, tagLine);
+
+        SpectatorDto spectatorDto = inGameService.callApiSpectator(checkPuuid);
+
+        AsyncCall<ChampionMastery, InGameView> asyncCall = new AsyncCall<>(spectatorDto.championMasteryList());
+
+        return asyncCall.execute(10, championMastery -> {
+            String puuid = championMastery.puuid();
+
+            byte[] championImage = championService.fetchChampionImage(championMastery.championId());
+
+            UserInfoDto userInfo = getUserInfo(puuid);
+
+            Champion champion = getInGameView(puuid, championMastery.championId());
+
+            return InGameView.of(
+                    championImage,
+                    userInfo.gameName(),
+                    userInfo.tagLine(),
+                    userInfo.tier(),
+                    userInfo.rank(),
+                    userInfo.summonerLevel(),
+                    userInfo.profileIconId(),
+                    championMastery.championLevel(),
+                    championMastery.championPoints(),
+                    champion
+            );
+        });
     }
 
-    private List<InGameView> asyncRequestInGame(String gameName, String tagLine) {
-        List<ChampionMastery> championMasteries = spectatorService.callApiSpectator(gameName, tagLine);
+    private String getPuuid(String gameName, String tagLine) {
+        return inGameRepository.findByGameNameAndTagLine(gameName, tagLine)
+                .orElseGet(() -> inGameService.callApiAccount(gameName, tagLine).puuid());
+    }
 
-        AsyncCall<ChampionMastery, InGameView> asyncCall = new AsyncCall<>(championMasteries);
+    private UserInfoDto getUserInfo(String puuid) {
+        Account account = inGameService.callApiAccount(puuid);
+        SummonerDto summonerDto = inGameService.callApiSummoner(account.puuid());
+        LeagueEntryDto leagueEntryDto = inGameService.callApiLeagueEntry(summonerDto.summonerId());
 
-        return null;
+        return UserInfoDto.of(leagueEntryDto, summonerDto, account);
+    }
+
+    private Champion getInGameView(String puuid, Long championId) {
+
+        List<SummonerPerformance> performances = inGameService
+                .callRiotSummonerPerformance(puuid)
+                .stream()
+                .filter(summonerPerformance -> Objects.equals(summonerPerformance.championId(), championId.intValue()))
+                .toList();
+
+
+        int killSum = performances.stream().mapToInt(SummonerPerformance::kills).sum();
+        int deathSum = performances.stream().mapToInt(SummonerPerformance::deaths).sum();
+        int assistSum = performances.stream().mapToInt(SummonerPerformance::assists).sum();
+        int recentGameCount = performances.size();
+        double kda = performances.stream().mapToDouble(SummonerPerformance::kda).sum();
+        int wins = (int) performances.stream().filter(SummonerPerformance::wins).count();
+        int losses = (int) performances.stream().filter(summonerPerformance -> !summonerPerformance.wins()).count();
+
+        return Champion.of(killSum, deathSum, assistSum, recentGameCount, kda / recentGameCount, wins, losses);
     }
 }
