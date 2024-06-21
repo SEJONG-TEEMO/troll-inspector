9cbe78f51b6b29e5cd7f50cd8c8df15bcf86392b
swager253 <swager253@naver.com>
2024-06-21 15:34:12 +0900

    feat: 400을 처리할 예외 클래스 생성

---
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/common/exception/RequestFailedException.java
@@ -0,0 +1,26 @@
+package sejong.teemo.ingamesearch.common.exception;
+
+public class RequestFailedException extends IllegalArgumentException {
+
+    private final ExceptionProvider exceptionProvider;
+
+    public RequestFailedException(ExceptionProvider exceptionProvider) {
+        super(exceptionProvider.getMessage());
+        this.exceptionProvider = exceptionProvider;
+    }
+
+    public RequestFailedException(String s, ExceptionProvider exceptionProvider) {
+        super(s);
+        this.exceptionProvider = exceptionProvider;
+    }
+
+    public RequestFailedException(String message, Throwable cause, ExceptionProvider exceptionProvider) {
+        super(message, cause);
+        this.exceptionProvider = exceptionProvider;
+    }
+
+    public RequestFailedException(Throwable cause, ExceptionProvider exceptionProvider) {
+        super(cause);
+        this.exceptionProvider = exceptionProvider;
+    }
+}
