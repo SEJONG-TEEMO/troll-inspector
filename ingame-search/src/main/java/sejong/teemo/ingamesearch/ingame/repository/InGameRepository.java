c243fa7e4887fd28d9a16deaf05d2085c5a1831e
swager253 <swager253@naver.com>
2024-06-21 17:41:40 +0900

    fix: query param 어노테이션을 추가하여 binding 문제 해결

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/repository/InGameRepository.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/repository/InGameRepository.java
@@ -2,6 +2,7 @@ package sejong.teemo.ingamesearch.ingame.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sejong.teemo.ingamesearch.ingame.entity.UserInfo;
 
 import java.util.Optional;
@@ -12,5 +13,6 @@ public interface InGameRepository extends JpaRepository<UserInfo, Long> {
 
     @Query("select ui.puuid from UserInfo ui " +
             "where ui.gameName = :gameName and ui.tagLine = :tagLine")
-    Optional<String> findByGameNameAndTagLine(String gameName, String tagLine);
+    Optional<String> findByGameNameAndTagLine(@Param("gameName") String gameName,
+                                              @Param("tagLine") String tagLine);
 }
