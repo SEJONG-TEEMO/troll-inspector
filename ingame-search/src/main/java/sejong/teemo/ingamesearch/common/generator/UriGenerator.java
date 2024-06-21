010432736df3b77a09827c8c8b51fe83c1ba8db6
swager253 <swager253@naver.com>
2024-06-21 14:58:35 +0900

    feat: uri account, spectator, summoner 추가

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/common/generator/UriGenerator.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/common/generator/UriGenerator.java
@@ -12,9 +12,11 @@ public enum UriGenerator {
     RIOT_API_LEAGUE("http://localhost:8080/teemo.gg/api/v1/league/{division}/{tier}/{queue}"),
     RIOT_API_LEAGUE_SUMMONER_ID("http://localhost:8080/teemo.gg/api/v1/league/{summonerId}"),
     RIOT_API_SUMMONER("http://localhost:8080/teemo.gg/api/v1//summoner/{encryptedSummonerId}"),
+    RIOT_API_SUMMONER_PUUID("http://localhost:8080/teemo.gg/api/v1/summoner/by-puuid/{puuid}"),
     RIOT_API_ACCOUNT("http://localhost:8080/teemo.gg/api/v1/account/{encryptedPuuid}"),
+    RIOT_API_ACCOUNT_GAME_NAME_AND_TAG_LINE("http://localhost:8080/teemo.gg/api/v1/account/{gameName}/{tagLine}"),
     RIOT_API_USER_INFO("http://localhost:8080/teemo.gg/api/v1/user-info/{division}/{tier}/{queue}"),
-    RIOT_API_SPECTATOR("http://localhost:8080/teemo.gg/api/v1/spectator/{gameName}/{tag}"),
+    RIOT_API_SPECTATOR("http://localhost:8080/teemo.gg/api/v1/spectator/{puuid}"),
     RIOT_API_SUMMONER_PERFORMANCE_GAME_NAME_TAG("http://localhost:8080/teemo.gg/api/v1/match/summoner-performance/{gameName}/{tag}"),
     RIOT_API_SUMMONER_PERFORMANCE_PUUID("http://localhost:8080/teemo.gg/api/v1/match/summoner-performance/{puuid}"),
 
