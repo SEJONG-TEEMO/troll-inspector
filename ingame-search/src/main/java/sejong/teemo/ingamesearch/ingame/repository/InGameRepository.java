43fb5429db931d5adfd35ed8a670003d475f5473
swager253 <swager253@naver.com>
2024-06-22 21:23:41 +0900

    feat: user info 조회 gameName, tag 를 이용한 조회 추가

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/repository/InGameRepository.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/repository/InGameRepository.java
@@ -11,8 +11,10 @@ public interface InGameRepository extends JpaRepository<UserInfo, Long> {
 
     boolean existsByGameNameAndTagLine(String gameName, String tagLine);
 
+    Optional<UserInfo> findByGameNameAndTagLine(String gameName, String tagLine);
+
     @Query("select ui.puuid from UserInfo ui " +
             "where ui.gameName = :gameName and ui.tagLine = :tagLine")
-    Optional<String> findByGameNameAndTagLine(@Param("gameName") String gameName,
-                                              @Param("tagLine") String tagLine);
+    Optional<String> findPuuidByGameNameAndTagLine(@Param("gameName") String gameName,
+                                                   @Param("tagLine") String tagLine);
 }
