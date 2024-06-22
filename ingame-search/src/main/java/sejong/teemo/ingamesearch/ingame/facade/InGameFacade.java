0303545cf900bc16edb4f5efb4d74ac400613a07
swager253 <swager253@naver.com>
2024-06-22 21:24:26 +0900

    refactor: SummonerPerformance 데이터 변경

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/facade/InGameFacade.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/facade/InGameFacade.java
@@ -2,18 +2,24 @@ package sejong.teemo.ingamesearch.ingame.facade;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import sejong.teemo.ingamesearch.champion.service.ChampionService;
 import sejong.teemo.ingamesearch.common.async.AsyncCall;
 import sejong.teemo.ingamesearch.ingame.dto.InGameView;
 import sejong.teemo.ingamesearch.ingame.dto.SpectatorDto;
 import sejong.teemo.ingamesearch.ingame.dto.champion.Champion;
 import sejong.teemo.ingamesearch.ingame.dto.champion.ChampionMastery;
+import sejong.teemo.ingamesearch.ingame.dto.normal.NormalView;
 import sejong.teemo.ingamesearch.ingame.dto.summoner.SummonerPerformance;
 import sejong.teemo.ingamesearch.ingame.dto.user.Account;
 import sejong.teemo.ingamesearch.ingame.dto.user.LeagueEntryDto;
 import sejong.teemo.ingamesearch.ingame.dto.user.SummonerDto;
 import sejong.teemo.ingamesearch.ingame.dto.user.UserInfoDto;
+import sejong.teemo.ingamesearch.ingame.entity.SummonerPerformanceInfo;
+import sejong.teemo.ingamesearch.ingame.entity.UserInfo;
 import sejong.teemo.ingamesearch.ingame.repository.InGameRepository;
+import sejong.teemo.ingamesearch.ingame.repository.QueryDslRepository;
+import sejong.teemo.ingamesearch.ingame.repository.SummonerPerformanceInfoRepository;
 import sejong.teemo.ingamesearch.ingame.service.InGameService;
 
 import java.util.List;
@@ -21,11 +27,14 @@ import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class InGameFacade {
 
     private final InGameService inGameService;
     private final ChampionService championService;
     private final InGameRepository inGameRepository;
+    private final SummonerPerformanceInfoRepository summonerPerformanceInfoRepository;
+    private final QueryDslRepository queryDslRepository;
 
     public List<InGameView> inGame(String gameName, String tagLine) {
 
@@ -59,9 +68,30 @@ public class InGameFacade {
         });
     }
 
+    @Transactional
+    public List<NormalView> normal(String gameName, String tagLine) {
+        UserInfo userInfo = getUserInfo(gameName, tagLine);
+
+        List<SummonerPerformanceInfo> summonerPerformanceInfos = inGameService.callRiotSummonerPerformance(userInfo.getPuuid())
+                .stream()
+                .map(summonerPerformance -> SummonerPerformanceInfo.of(summonerPerformance, userInfo))
+                .toList();
+
+        if(!summonerPerformanceInfoRepository.existsByUserInfoId(userInfo.getId())) {
+            summonerPerformanceInfoRepository.saveAll(summonerPerformanceInfos);
+        }
+
+        return queryDslRepository.findRecentGamesByUserId(userInfo.getId());
+    }
+
     private String getPuuid(String gameName, String tagLine) {
-        return inGameRepository.findByGameNameAndTagLine(gameName, tagLine)
+        return inGameRepository.findPuuidByGameNameAndTagLine(gameName, tagLine)
                 .orElseGet(() -> inGameService.callApiAccount(gameName, tagLine).puuid());
+    }
+
+    private UserInfo getUserInfo(String gameName, String tagLine) {
+        return inGameRepository.findByGameNameAndTagLine(gameName, tagLine)
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
     }
 
     private UserInfoDto getUserInfo(String puuid) {
