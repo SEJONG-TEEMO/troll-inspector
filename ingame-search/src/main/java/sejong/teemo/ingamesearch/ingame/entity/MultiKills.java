d0638d87e424595a07f90df6e76355d205d3dd37
swager253 <swager253@naver.com>
2024-06-22 21:22:23 +0900

    feat: kill 데이터의 책임 분리

---
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/entity/MultiKills.java
@@ -0,0 +1,38 @@
+package sejong.teemo.ingamesearch.ingame.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MultiKills {
+
+    @Column(name = "penta_kills")
+    private int pentaKills;
+
+    @Column(name = "quadra_kills")
+    private int quadraKiils;
+
+    @Column(name = "triple_kills")
+    private int tripleKills;
+
+    @Builder
+    private MultiKills(int pentaKills, int quadraKiils, int tripleKills) {
+        this.pentaKills = pentaKills;
+        this.quadraKiils = quadraKiils;
+        this.tripleKills = tripleKills;
+    }
+
+    public static MultiKills of(int pentaKills, int quadraKiils, int tripleKills) {
+        return MultiKills.builder()
+                .pentaKills(pentaKills)
+                .quadraKiils(quadraKiils)
+                .tripleKills(tripleKills)
+                .build();
+    }
+}
