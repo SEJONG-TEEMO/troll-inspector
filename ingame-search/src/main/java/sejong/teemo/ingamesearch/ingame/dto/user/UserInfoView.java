1dcde703c6948d0686f3e84e6c3dfd6671fa2213
swager253 <swager253@naver.com>
2024-06-23 16:55:16 +0900

    feat: gameName, tagLine, total 데이터 추가

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/dto/user/UserInfoView.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/dto/user/UserInfoView.java
@@ -1,10 +1,28 @@
 package sejong.teemo.ingamesearch.ingame.dto.user;
 
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
+
+@Builder
 public record UserInfoView(
-        byte[] profileIconImage,
+        String gameName,
+        String tagLine,
         String tier,
         String rank,
+        int total,
         int wins,
-        int losses
-) {
+        int losses,
+        int profileIconId) {
+
+    @QueryProjection
+    public UserInfoView(String gameName, String tagLine, String tier, String rank, int total, int wins, int losses, int profileIconId) {
+        this.gameName = gameName;
+        this.tagLine = tagLine;
+        this.tier = tier;
+        this.rank = rank;
+        this.total = total;
+        this.wins = wins;
+        this.losses = losses;
+        this.profileIconId = profileIconId;
+    }
 }
