38386185b0d924e9edb92cc8aa2e0a01d8e3fc97
swager253 <swager253@naver.com>
2024-06-23 21:45:08 +0900

    feat(QueryDslRepository): performance update 쿼리 메서드 추가

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/repository/QueryDslRepository.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/repository/QueryDslRepository.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import sejong.teemo.ingamesearch.ingame.dto.normal.NormalView;
 import sejong.teemo.ingamesearch.ingame.dto.normal.QNormalView;
+import sejong.teemo.ingamesearch.ingame.dto.summoner.SummonerPerformance;
 import sejong.teemo.ingamesearch.ingame.dto.user.QUserInfoView;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static sejong.teemo.ingamesearch.ingame.entity.QSummonerPerformanceInfo.*;
@@ -24,8 +26,12 @@ public class QueryDslRepository {
         return queryFactory.select(
                 new QNormalView(
                         new QUserInfoView(
+                                summonerPerformanceInfo.userInfo.gameName,
+                                summonerPerformanceInfo.userInfo.tagLine,
                                 summonerPerformanceInfo.userInfo.tier,
                                 summonerPerformanceInfo.userInfo.rank,
+                                summonerPerformanceInfo.userInfo.wins
+                                        .add(summonerPerformanceInfo.userInfo.losses),
                                 summonerPerformanceInfo.userInfo.wins,
                                 summonerPerformanceInfo.userInfo.losses,
                                 summonerPerformanceInfo.userInfo.profileIconId
@@ -34,25 +40,48 @@ public class QueryDslRepository {
                         summonerPerformanceInfo.championId.count().intValue(),
                         getWinRate(),
                         summonerPerformanceInfo.kda.avg(),
-                        summonerPerformanceInfo.kills.sum(),
-                        summonerPerformanceInfo.deaths.sum(),
-                        summonerPerformanceInfo.assists.sum(),
+                        summonerPerformanceInfo.kills.avg(),
+                        summonerPerformanceInfo.deaths.avg(),
+                        summonerPerformanceInfo.assists.avg(),
                         summonerPerformanceInfo.totalMinionKills.avg(),
                         summonerPerformanceInfo.multiKills.pentaKills.sum(),
                         summonerPerformanceInfo.multiKills.quadraKiils.sum(),
                         summonerPerformanceInfo.multiKills.tripleKills.sum(),
-                        summonerPerformanceInfo.championWins.eq(true).count().intValue(),
-                        summonerPerformanceInfo.championWins.eq(false).count().intValue()
+                        getWinCount(true).intValue(),
+                        getWinCount(false).intValue()
                 )).from(summonerPerformanceInfo)
                 .where(summonerPerformanceInfo.userInfo.id.eq(userId))
                 .groupBy(summonerPerformanceInfo.championId)
-                .orderBy(getWinRate().desc())
+                .orderBy(summonerPerformanceInfo.championId.count().desc())
                 .fetch();
 
     }
 
+    @Transactional
+    public long updateSummonerPerformanceInfo(SummonerPerformance performances, Long userInfoId) {
+        return queryFactory.update(summonerPerformanceInfo)
+                .set(summonerPerformanceInfo.championId, performances.championId())
+                .set(summonerPerformanceInfo.kda, performances.kda())
+                .set(summonerPerformanceInfo.killParticipation, performances.killParticipation())
+                .set(summonerPerformanceInfo.kills, performances.kills())
+                .set(summonerPerformanceInfo.deaths, performances.deaths())
+                .set(summonerPerformanceInfo.assists, performances.assists())
+                .set(summonerPerformanceInfo.totalMinionKills, performances.totalMinionsKilled())
+                .set(summonerPerformanceInfo.multiKills.pentaKills, performances.pentaKills())
+                .set(summonerPerformanceInfo.multiKills.quadraKiils, performances.quadraKills())
+                .set(summonerPerformanceInfo.multiKills.tripleKills, performances.tripleKills())
+                .set(summonerPerformanceInfo.championWins, performances.win())
+                .set(summonerPerformanceInfo.updateAt, LocalDateTime.now())
+                .where(summonerPerformanceInfo.userInfo.id.eq(userInfoId))
+                .execute();
+    }
+
+    private static NumberExpression<Integer> getWinCount(boolean check) {
+        return summonerPerformanceInfo.championWins.when(check).then(1).otherwise(0).sum();
+    }
+
     private static NumberExpression<Double> getWinRate() {
-        return summonerPerformanceInfo.championWins.eq(true).count().doubleValue()
-                .divide(summonerPerformanceInfo.championId.count()).doubleValue();
+        return getWinCount(true).doubleValue()
+                .divide(summonerPerformanceInfo.championId.count().doubleValue()).multiply(100);
     }
 }
