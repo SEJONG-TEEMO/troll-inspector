e5ef96286ac52497ec5e7b30e3de37d3cf74f90b
swager253 <swager253@naver.com>
2024-06-23 21:48:12 +0900

    feat(NormalView): query dsl projection 추가

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/dto/normal/NormalView.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/ingame/dto/normal/NormalView.java
@@ -1,22 +1,42 @@
 package sejong.teemo.ingamesearch.ingame.dto.normal;
 
-import sejong.teemo.ingamesearch.ingame.dto.user.UserInfoDto;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
 import sejong.teemo.ingamesearch.ingame.dto.user.UserInfoView;
 
+@Builder
 public record NormalView(
         UserInfoView userInfoView,
-        byte[] championImage,
+        int championId,
+        int recentGameCount,
         double winRate,
         double kda,
-        int kills,
-        int deaths,
-        int assists,
+        double kills,
+        double deaths,
+        double assists,
         double cs,
-        double averageGold,
         int pentaKill,
         int quadraKill,
         int tripleKill,
         int wins,
         int losses
 ) {
+
+    @QueryProjection
+    public NormalView(UserInfoView userInfoView, int championId, int recentGameCount, double winRate, double kda, double kills, double deaths, double assists, double cs, int pentaKill, int quadraKill, int tripleKill, int wins, int losses) {
+        this.userInfoView = userInfoView;
+        this.championId = championId;
+        this.recentGameCount = recentGameCount;
+        this.winRate = winRate;
+        this.kda = kda;
+        this.kills = kills;
+        this.deaths = deaths;
+        this.assists = assists;
+        this.cs = cs;
+        this.pentaKill = pentaKill;
+        this.quadraKill = quadraKill;
+        this.tripleKill = tripleKill;
+        this.wins = wins;
+        this.losses = losses;
+    }
 }
