8598c03849fefa16a239eefcc6509dd664f93cdb
swager253 <swager253@naver.com>
2024-06-23 20:55:47 +0900

    test(InGameFacadeTest): 테스트 이름 변경

--- ingame-search/src/test/java/sejong/teemo/ingamesearch/ingame/integration/facade/InGameFacadeTest.java
+++ ingame-search/src/test/java/sejong/teemo/ingamesearch/ingame/integration/facade/InGameFacadeTest.java
@@ -5,14 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import sejong.teemo.ingamesearch.container.TestContainer;
 import sejong.teemo.ingamesearch.ingame.dto.InGameView;
-import sejong.teemo.ingamesearch.ingame.dto.normal.NormalView;
+import sejong.teemo.ingamesearch.ingame.dto.user.performance.UserPerformanceDto;
 import sejong.teemo.ingamesearch.ingame.entity.UserInfo;
 import sejong.teemo.ingamesearch.ingame.facade.InGameFacade;
 import sejong.teemo.ingamesearch.ingame.repository.InGameRepository;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class InGameFacadeTest extends TestContainer {
@@ -37,7 +35,7 @@ class InGameFacadeTest extends TestContainer {
     }
 
     @Test
-    void 최종_NORMAL_통합_테스트() {
+    void 최종_viewUserGamePerformance_통합_테스트() {
         // given
         String gameName = "a1h";
         String tag = "KR1";
@@ -59,7 +57,7 @@ class InGameFacadeTest extends TestContainer {
                 30L));
 
         // when
-        List<NormalView> normal = inGameFacade.normal(gameName, tag);
+        UserPerformanceDto normal = inGameFacade.viewUserGamePerformance(gameName, tag);
 
         // then
         System.out.println(normal);
