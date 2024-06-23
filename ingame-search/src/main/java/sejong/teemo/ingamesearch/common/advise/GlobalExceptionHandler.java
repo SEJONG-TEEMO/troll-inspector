b36c97869f9f9c64b93694ed94222fdc59c8a7ad
swager253 <swager253@naver.com>
2024-06-23 16:54:18 +0900

    feat: 잘못된 정보에 따라 예외 처리

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/common/advise/GlobalExceptionHandler.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/common/advise/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package sejong.teemo.ingamesearch.common.advise;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -14,6 +17,12 @@ import sejong.teemo.ingamesearch.common.exception.TooManyApiCallingException;
 @Slf4j
 @RestControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ErrorResponseException(HttpStatus.BAD_REQUEST, ex);
+    }
 
     @ExceptionHandler(FailedApiCallingException.class)
     protected ErrorResponse handleFailedApiCallingException(FailedApiCallingException e) {
