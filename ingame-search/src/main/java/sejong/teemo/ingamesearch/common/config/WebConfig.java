f7901b0b2d1f8fc58cb6a51fc9a67db0de5e5ad1
swager253 <swager253@naver.com>
2024-06-25 21:47:43 +0900

    feat: WebConfig rate limiter interceptor 등록

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/common/config/WebConfig.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/common/config/WebConfig.java
@@ -1,13 +1,19 @@
 package sejong.teemo.ingamesearch.common.config;
 
+import io.github.bucket4j.Bucket;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import sejong.teemo.ingamesearch.common.interceptor.InGameInterceptor;
+import sejong.teemo.ingamesearch.common.interceptor.RateLimiterInterceptor;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final Bucket bucket;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -21,6 +27,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new InGameInterceptor())
+                .addPathPatterns("/**");
+
+        registry.addInterceptor(new RateLimiterInterceptor(bucket))
                 .addPathPatterns("/**");
     }
 }
