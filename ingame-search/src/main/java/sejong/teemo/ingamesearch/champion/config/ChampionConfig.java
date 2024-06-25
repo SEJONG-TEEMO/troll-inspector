87a5037c3f72ef4c25119c200f92a9b3191128f0
swager253 <swager253@naver.com>
2024-06-25 16:46:46 +0900

    remove(ChampionConfig): 불필요한 클래스 삭제

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/champion/config/ChampionConfig.java
+++
@@ -1,9 +0,0 @@
-package sejong.teemo.ingamesearch.champion.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ChampionConfig {
-
-}
