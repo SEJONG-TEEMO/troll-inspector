3aeca8eedbd4d624c0b906e2ae5ca29ea3af2fa9
swager253 <swager253@naver.com>
2024-06-25 21:49:10 +0900

    front refactor.

--- troll-inspector-view/src/main/InGameSearchBar.jsx
+++ troll-inspector-view/src/main/InGameSearchBar.jsx
@@ -1,29 +1,42 @@
 import React, {useState} from "react";
 import {Button, Input} from "@nextui-org/react";
 import {SearchIcon} from "./SearchIcon.jsx";
-import {trollInspectorNameAndTag} from "../axios/search.js";
+import {inGameNameAndTag} from "../axios/search.js";
 
-export function TrollInspectorBar() {
+export function InGameSearchBar() {
 
     const [search, setSearch] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [searchResult, setSearchResult] = useState([]);
 
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
             event.preventDefault();
-            trollInspectorNameAndTag(event.target.value);
+            inGameNameAndTag(event.target.value);
             setSearch('');
         }
     };
+
+    const handleSearch = (search) => {
+        const res = inGameNameAndTag(search);
+        res.then(response => {
+            setSearchResult(response.data)
+            setIsModalOpen(true);
+        }).catch(error => {
+            alert("데이터를 가져오지 못했습니다.")
+            console.error(error)
+        });
+    }
 
     return (
         <div className="w-screen h-screen p-8 flex items-center justify-center">
             <Input
                 className="max-w-2xl"
                 type="search"
-                label="검색"
+                label="인게임 검색"
                 isClearable
                 radius="lg"
-                placeholder="소환사 이름 + #태그를 입력해주세요."
+                placeholder="자신의 소환사 이름 + #태그를 입력해주세요. (솔랭 기준)"
                 startContent={
                     <SearchIcon
                         className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0"/>
@@ -34,7 +47,7 @@ export function TrollInspectorBar() {
                 }}
                 onKeyDown={handleKeyDown}
             />
-            <Button type="submit" isIconOnly size={"lg"} onClick={trollInspectorNameAndTag}>
+            <Button type="submit" isIconOnly size={"lg"} onClick={inGameNameAndTag}>
                 <SearchIcon/>
             </Button>
         </div>
