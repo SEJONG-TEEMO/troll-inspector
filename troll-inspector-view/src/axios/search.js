52b58bebe1f1a85be7476d9d1589d68d5e7cfeea
swager253 <swager253@naver.com>
2024-06-26 20:46:01 +0900

    fix: search.js patch 누락된 부분 fix

--- troll-inspector-view/src/axios/search.js
+++ troll-inspector-view/src/axios/search.js
@@ -40,13 +40,14 @@ export async function inGameNameAndTag(search) {
 
 export async function updateUserPerformance(updateData) {
 
+    console.log(updateData);
     if(updateData.includes("#")) {
         const split = updateData.split("#");
         const name = split[0].trim();
         const tag = split[1].trim();
 
         console.log(name, tag);
-        return await axios.patch(baseUrl + searchUrl, {params: {gameName: name, tagLine: tag}});
+        return await axios.patch(baseUrl + searchUrl, null, {params: {gameName: name, tagLine: tag}});
     } else {
         alert("잘못된 입력입니다.")
     }
