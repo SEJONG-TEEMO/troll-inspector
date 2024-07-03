ae30c5e56f76ae5b6be0f5bf5ec847f277ee43c6
swager253 <swager253@naver.com>
2024-07-03 21:45:28 +0900

    refactor: UriGenerator 주소 변경

--- ingame-search/src/main/java/sejong/teemo/ingamesearch/common/generator/UriGenerator.java
+++ ingame-search/src/main/java/sejong/teemo/ingamesearch/common/generator/UriGenerator.java
@@ -8,17 +8,17 @@ import java.net.URI;
 @Getter
 public enum UriGenerator {
 
-    RIOT_API_LEAGUES("http://localhost:8080/teemo.gg/api/v1/league-to-summoner/{division}/{tier}/{queue}"),
-    RIOT_API_LEAGUE("http://localhost:8080/teemo.gg/api/v1/league/{division}/{tier}/{queue}"),
-    RIOT_API_LEAGUE_SUMMONER_ID("http://localhost:8080/teemo.gg/api/v1/league/{summonerId}"),
-    RIOT_API_SUMMONER("http://localhost:8080/teemo.gg/api/v1//summoner/{encryptedSummonerId}"),
-    RIOT_API_SUMMONER_PUUID("http://localhost:8080/teemo.gg/api/v1/summoner/by-puuid/{puuid}"),
-    RIOT_API_ACCOUNT("http://localhost:8080/teemo.gg/api/v1/account/{encryptedPuuid}"),
-    RIOT_API_ACCOUNT_GAME_NAME_AND_TAG_LINE("http://localhost:8080/teemo.gg/api/v1/account/{gameName}/{tagLine}"),
-    RIOT_API_USER_INFO("http://localhost:8080/teemo.gg/api/v1/user-info/{division}/{tier}/{queue}"),
-    RIOT_API_SPECTATOR("http://localhost:8080/teemo.gg/api/v1/spectator/{puuid}"),
-    RIOT_API_SUMMONER_PERFORMANCE_GAME_NAME_TAG("http://localhost:8080/teemo.gg/api/v1/match/summoner-performance/{gameName}/{tag}"),
-    RIOT_API_SUMMONER_PERFORMANCE_PUUID("http://localhost:8080/teemo.gg/api/v1/match/summoner-performance/{puuid}"),
+    RIOT_API_LEAGUES("https://riot-api-kscupel2fq-du.a.run.app/teemo.gg/api/v1/league-to-summoner/{division}/{tier}/{queue}"),
+    RIOT_API_LEAGUE("https://riot-api-kscupel2fq-du.a.run.app/teemo.gg/api/v1/league/{division}/{tier}/{queue}"),
+    RIOT_API_LEAGUE_SUMMONER_ID("https://riot-api-kscupel2fq-du.a.run.app/teemo.gg/api/v1/league/{summonerId}"),
+    RIOT_API_SUMMONER("https://riot-api-kscupel2fq-du.a.run.app/teemo.gg/api/v1//summoner/{encryptedSummonerId}"),
+    RIOT_API_SUMMONER_PUUID("https://riot-api-kscupel2fq-du.a.run.app/teemo.gg/api/v1/summoner/by-puuid/{puuid}"),
+    RIOT_API_ACCOUNT("https://riot-api-kscupel2fq-du.a.run.app/teemo.gg/api/v1/account/{encryptedPuuid}"),
+    RIOT_API_ACCOUNT_GAME_NAME_AND_TAG_LINE("https://riot-api-kscupel2fq-du.a.run.app/teemo.gg/api/v1/account/{gameName}/{tagLine}"),
+    RIOT_API_USER_INFO("https://riot-api-kscupel2fq-du.a.run.app/teemo.gg/api/v1/user-info/{division}/{tier}/{queue}"),
+    RIOT_API_SPECTATOR("https://riot-api-kscupel2fq-du.a.run.app/teemo.gg/api/v1/spectator/{puuid}"),
+    RIOT_API_SUMMONER_PERFORMANCE_GAME_NAME_TAG("https://riot-api-kscupel2fq-du.a.run.app/teemo.gg/api/v1/match/summoner-performance/{gameName}/{tag}"),
+    RIOT_API_SUMMONER_PERFORMANCE_PUUID("https://riot-api-kscupel2fq-du.a.run.app/teemo.gg/api/v1/match/summoner-performance/{puuid}"),
 
     CHAMPION_IMAGE_CDN("https://ddragon.leagueoflegends.com/cdn/14.12.1/img/champion/{championName}.png"),
 
